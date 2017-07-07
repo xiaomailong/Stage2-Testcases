d4bae8af78e92fc5058cc138c64e885db719ecaa
Johan Sonesson <johan@traintech.eu>
2017-07-07 17:45:41 +0100

    Created Testcase Base for all testcases to use, as well as a template

---
+++ Testcase/TestCaseTemplate.cs
@@ -0,0 +1,47 @@
+﻿#region usings
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BT_Tools;
+using BT_CSB_Tools;
+using BT_CSB_Tools.Logging;
+using BT_CSB_Tools.Utils.Xml;
+using BT_CSB_Tools.SignalPoolGenerator.Signals;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+using CL345;
+
+#endregion
+
+namespace Testcase
+{
+    public class TestCaseTemplate : TestcaseBase
+    {
+        public override void PreExecution()
+        {
+            // Setup test
+            
+            // Call the TestCaseBase PreExecution
+            base.PreExecution();
+        }
+
+        public override void PostExecution()
+        {
+            // Do post actions
+
+            // Call the TestCaseBase PostExecution
+            base.PostExecution();
+        }
+
+        public override bool TestcaseEntryPoint()
+        {
+
+            return GlobalTestResult;
+        }
+    }
+}
--- Testcase/TestcaseBase.cs
+++ Testcase/TestcaseBase.cs
@@ -20,11 +20,16 @@ using CL345;
 
 namespace Testcase
 {
-    public class Testcase : SignalPool
+    public class TestcaseBase : SignalPool
     {
         public override void PreExecution()
         {
-			// Pre-test configuration.
+            // Pre-test configuration.
+
+            // Set up safe words
+            SITR.CCUS.ETCSTrTelegram1.Tr1SSW1.Value = 0x8000;
+            SITR.CCUS.ETCSTrTelegram1.Tr1SSW2.Value = 0x8000;
+            SITR.CCUS.ETCSTrTelegram1.Tr1SSW3.Value = 0x8000;
         }
 
         public override void PostExecution()
@@ -32,11 +37,15 @@ namespace Testcase
 			// Post-test cleanup.
         }
 
+        /// <summary>
+        /// Testcase entry point, as this is the base, this method should be overriden and never called
+        /// </summary>
+        /// <returns></returns>
         public override bool TestcaseEntryPoint()
         {
-			// Test case entry point.
-			return GlobalTestResult;
-		}
+			// As this method should never be called, throw an exception
+            throw new InvalidOperationException("The TestcaseEntryPoint method on TestcaseBase should never be called, it should be overriden");
+        }
 
 		public override void RunDebugger()
         {
