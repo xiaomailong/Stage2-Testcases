5807c874c58426922b45a7b345e395316110d5b6
ssivahar <ssivahar@BTIS1000057806>
2017-07-24 15:52:47 +0100

    SSI - 24/07/2017: EVC101 packet checking class added

---
+++ Testcase/Telegrams/DMItoEVC/EVC101_MMIDriverRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using CL345;
+
+namespace Testcase.Telegrams
+{
+    /// <summary>
+    /// This packet shall be sent when the driver requests for an action from the ATP, 
+    /// typically by pressing a button..
+    /// </summary>
+    static class EVC101_MMIDriverRequest
+    {
+        private static SignalPool _pool;        
+
+        public static void Receive(byte mmiMRequest, bool mmiQButton)
+        {
+            // Checking packet id
+            _pool.SITR.CCUO.ETCS1DriverRequest.MmiMRequest.Equals(101);
+            // Checking packet length
+            _pool.SITR.CCUO.ETCS1DriverRequest.MmiLPacket.Equals(80);
+            // Checking MMI_M_REQUEST
+            _pool.SITR.CCUO.ETCS1DriverRequest.MmiMRequest.Equals(mmiMRequest);
+            // Extracting EVC101alias1 into an array of byte
+            BitArray evc101alias1 = new BitArray(new[]
+            { _pool.SITR.CCUO.ETCS1DriverRequest.EVC101alias1.Value });
+            // Checking bool MMI_Q_BUTTON
+            bool qButton = evc101alias1[7].Equals(mmiQButton);
+        }
+    }
+}
