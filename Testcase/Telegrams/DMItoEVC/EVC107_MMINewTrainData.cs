bd27b04a285f6c2bd7431c23be84dbb181aa9fb5
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-02 15:31:35 +0100

    Added reference to SignalPool in the method signature. Otherwise, the "_pool" would never be assigned a variable.

--- Testcase/Telegrams/DMItoEVC/EVC107_MMINewTrainData.cs
+++ Testcase/Telegrams/DMItoEVC/EVC107_MMINewTrainData.cs
@@ -9,32 +9,38 @@ namespace Testcase.Telegrams
 {
     /// <summary>
     /// This packet shall be sent when the driver acts during the Train Data Entry procedure. It covers the following use cases:
-    ///1.) Driver accepts a data element by pressing 'Enter'.
-    ///2.) Driver accepts a data element by pressing 'Enter_Delay_Type'.
-    ///3.) Driver completes entering a data block by pressing 'Yes'
-    ///4.) Driver overrules an operational check rule by pressing 'Delay Type Yes'
+    /// 1. Driver accepts a data element by pressing 'Enter'.
+    /// 2. Driver accepts a data element by pressing 'Enter_Delay_Type'.
+    /// 3. Driver completes entering a data block by pressing 'Yes'.
+    /// 4. Driver overrules an operational check rule by pressing 'Delay Type Yes'.
     /// </summary>
     static class EVC107_MMINewTrainData
     {
         private static SignalPool _pool;
 
-        public static void ReceiveVariableTD(ushort mmiLTrain, ushort mmiVMaxTrain)
+        public static void ReceiveVariableTD(ushort mmiLTrain, ushort mmiVMaxTrain, SignalPool pool)
         {
-            bool bResult = false;
+            bool Result = false;
 
-            // Checking packet id
+            _pool = pool;
+
+            // Check packet ID
             _pool.SITR.CCUO.ETCS1NewTrainData.MmiMPacket.Value.Equals(107);
-            // Checking MMI_L_TRAIN
-            bResult = _pool.SITR.CCUO.ETCS1NewTrainData.MmiLTrain.Value.Equals(mmiLTrain);
-            if (bResult)
+            
+            // Check MMI_L_TRAIN
+            Result = _pool.SITR.CCUO.ETCS1NewTrainData.MmiLTrain.Value.Equals(mmiLTrain);
+
+            if (Result)
             {
-                _pool.TraceInfo("EVC-107 received: MMI_L_TRAIN = {0}", mmiLTrain);
+                _pool.TraceInfo($"EVC-107 received: MMI_L_TRAIN = {mmiLTrain}");
             }
-            // Checking MMI_V_MAXTRAIN
-            bResult = _pool.SITR.CCUO.ETCS1NewTrainData.MmiVMaxtrain.Value.Equals(mmiVMaxTrain);
-            if (bResult)
+            
+            // Check MMI_V_MAXTRAIN
+            Result = _pool.SITR.CCUO.ETCS1NewTrainData.MmiVMaxtrain.Value.Equals(mmiVMaxTrain);
+
+            if (Result)
             {
-                _pool.TraceInfo("EVC-107 received: MMI_V_MAXTRAIN = {0}", mmiVMaxTrain);
+                _pool.TraceInfo($"EVC-107 received: MMI_V_MAXTRAIN = {mmiVMaxTrain}");
             }
         }
     }
