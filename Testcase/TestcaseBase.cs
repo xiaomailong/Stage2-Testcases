5bd0ad30870e22d556455dc9eafeb1c634d01b91
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-02 15:32:53 +0100

    Added initialisation of EVC telegrams and setting of dynamic array sending so that they will always be available in all test cases.

--- Testcase/TestcaseBase.cs
+++ Testcase/TestcaseBase.cs
@@ -16,6 +16,7 @@ using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
 using CL345;
+using Testcase.Telegrams.EVCtoDMI;
 
 #endregion
 
@@ -25,7 +26,31 @@ namespace Testcase
     {
         public override void PreExecution()
         {
-            // Pre-test configuration.           
+            // Pre-test configuration.
+
+            // Initialise instance of all telegrams
+            TraceInfo("Initialise Default Values");
+            EVC0_MMIStartATP.Initialise(this);
+            EVC1_MMIDynamic.Initialise(this);
+            EVC2_MMIStatus.Initialise(this);
+            EVC3_MMISetTimeATP.Initialise(this);
+            EVC5_MMIGeoPosition.Initialise(this);
+            EVC6_MMICurrentTrainData.Initialise(this);
+            EVC7_MMIEtcsMiscOutSignals.Initialise(this);
+            EVC8_MMIDriverMessage.Initialise(this);
+            EVC14_MMICurrentDriverID.Initialise(this);
+            EVC16_CurrentTrainNumber.Initialise(this);
+            EVC22_MMICurrentRBC.Initialise(this);
+            EVC30_MMIRequestEnable.Initialise(this);
+            EVC34_MMISystemVersion.Initialise(this);
+
+            // Initialises all EVC packets that contain dynamic arrays
+            SITR.SMDCtrl.ETCS1.SelectLevel.Value = 0x8;
+            SITR.SMDCtrl.ETCS1.SetVbc.Value = 0x8;
+            SITR.SMDCtrl.ETCS1.RemoveVbc.Value = 0x8;
+            SITR.SMDCtrl.ETCS1.TrackDescription.Value = 0x8;
+            SITR.SMDCtrl.ETCS1.CurrentRbcData.Value = 0x8;
+            SITR.SMDCtrl.ETCS1.EchoedTrainData.Value = 0x8;
         }
 
         public override void PostExecution()
