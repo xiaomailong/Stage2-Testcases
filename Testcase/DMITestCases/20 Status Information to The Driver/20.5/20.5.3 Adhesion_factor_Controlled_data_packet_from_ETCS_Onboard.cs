b66e2ce55dbbfacfd598b71b9c14178c7c56e1a1
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-10 15:14:27 +0100

    TC 15.5.2 completed

--- Testcase/DMITestCases/20 Status Information to The Driver/20.5/20.5.3 Adhesion_factor_Controlled_data_packet_from_ETCS_Onboard.cs
+++ Testcase/DMITestCases/20 Status Information to The Driver/20.5/20.5.3 Adhesion_factor_Controlled_data_packet_from_ETCS_Onboard.cs
@@ -13,6 +13,7 @@ using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
 using CL345;
+using Testcase.DMITestCases;
 
 namespace Testcase.DMITestCases
 {
@@ -20,18 +21,26 @@ namespace Testcase.DMITestCases
     /// 20.5.3 Adhesion factor: Controlled data packet from ETCS Onboard
     /// TC-ID: 15.5.2
     /// 
-    /// This test case verifies the display of the adhesion factor indication on DMI when the factor is activated by both driver and Trackside together, which the condition is complied with [MMI-ETCS-gen], [MMIIS] and [ERA-ERTMS] standard.
+    /// This test case verifies the display of the adhesion factor indication on DMI when the factor is activated by both driver
+    /// and Trackside together.
     /// 
     /// Tested Requirements:
     /// MMI_gen 7088; MMI_gen 600; MMI_gen 8435;
     /// 
     /// Scenario:
-    /// Drive the train passes BG0 at position 100m: pkt 3 Q_NVDRIVER_ADHES=0 (to reset the value if it has been set)Drive the train passes BG1 at position 250m: pkt 3 Q_NVDRIVER_ADHES=1The adhesion factor indication is verified with the following events: (Redundant Case)Activate Slippery button at Adhesion window.The ‘Adhesion’ button is revoked when drive the train passes BG2 at position 600m: pkt 71 D_ADHESION=0, L_ADHESION=200, M_ADHESION=0 (Slippery)Drive the train passes length of reduced adhesion at 800m.Deactivates Slippery button.
+    /// Drive the train past BG0 at position 100 m: packet 3 Q_NVDRIVER_ADHES = 0 (to reset the value if it has been set)
+    /// Drive the train past BG1 at position 250 m: packet 3 Q_NVDRIVER_ADHES = 1
+    /// 
+    /// The adhesion factor indication is verified with the following events:
+    /// 1. (Redundant Case) Activate Slippery button at Adhesion window.
+    /// The ‘Adhesion’ button is revoked when the train passes BG2 at position 600 m:
+    ///     packet 71 D_ADHESION = 0, L_ADHESION = 200, M_ADHESION = 0 (Slippery)
+    /// Drive the train past length of reduced adhesion at 800 m. Deactivates Slippery button.
     /// 
     /// Used files:
     /// 15_5_2.tdg
     /// </summary>
-    public class Adhesion_factor_Controlled_data_packet_from_ETCS_Onboard : TestcaseBase
+    public class TC_15_5_2_Adhesion_Factor : TestcaseBase
     {
         public override void PreExecution()
         {
@@ -54,65 +63,47 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // Testcase entrypoint
-
+            DmiExpectedResults.Testcase_not_required(this, "15.5.1", "20.5.1");
 
             /*
             Test Step 1
             Action: Driver drives the train forward passing BG1.Then, press ‘Special’ button
             Expected Result: DMI still displays in SR mode.Verify that ‘Adhesion’ button is enabled
             */
-            // Call generic Check Results Method
-            DmiExpectedResults.DMI_displays_Special_window_with_enabled_Adhesion_button(this);
-
+            
             /*
             Test Step 2
             Action: Press ‘Adhesion’ button.Then, press ‘Slippery rail’ button
             Expected Result: Verify the following information,Use the log file to confirm that DMI receives EVC-2 with variable MMI_M_ADHESION (#0) = 1, bit ‘Low Adhesion by Driver’ is set.DMI displays symbol ST02 in sub-area A4, by driver
             Test Step Comment: (1) MMI_gen 7088 (partly: EVC-2, ‘Low Adhesion by Driver’)(2) MMI_gen 111;     
             */
-
-
+            
             /*
             Test Step 3
             Action: Drive the train forward passing BG2
             Expected Result: Verify the following information,Use the log file to confirm that DMI receives EVC-2 with variable MMI_M_ADHESION (#1) = 1, bit ‘Low Adhesion from Trackside’ is set.DMI displays symbol ST02 in sub-area A4
             Test Step Comment: (1) MMI_gen 7088 (partly: EVC-2, ‘Low Adhesion from Trackside’, ‘Low Adhesion by Driver’);(2) MMI_gen 111;
             */
-            // Call generic Action Method
-            DmiActions.Drive_train_forward_passing_BG2(this);
-            // Call generic Check Results Method
-
-            //DmiExpectedResults
-            //    .Verify_the_following_information_Use_the_log_file_to_confirm_that_DMI_receives_EVC_2_with_variable_MMI_M_ADHESION_1_1_bit_Low_Adhesion_from_Trackside_is_set_DMI_displays_symbol_ST02_in_sub_area_A4(this);
-
-
+            
             /*
             Test Step 4
             Action: Drive the train forward
             Expected Result: Verify the following information,Use the log file to confirm that DMI receives EVC-2 with following variable,MMI_M_ADHESION (#1) = 0, bit ‘Low Adhesion from Trackside’ is not set.DMI displays symbol ST02 in sub-area A4, by driver
             Test Step Comment: (1) MMI_gen 7088 (partly: EVC-2, ‘Low Adhesion by Driver’)(2) MMI_gen 111;     
             */
-            // Call generic Action Method
-            DmiActions.Drive_the_train_forward(this);
-
-
+            
             /*
             Test Step 5
             Action: Perform the following procedure,Press ‘Special’ button.Press ‘Adhesion’ button.Select and confirm ‘Non slippery rail’ button
             Expected Result: No adhesion factor indication is displayed.Verify the following information,Use the log file to confirm that DMI receives EVC-2 with following variable,MMI_M_ADHESION (#0) = 0, bit ‘Low Adhesion by Driver’ is not set
             Test Step Comment: (1) MMI_gen 7088 (partly: No symbol displayed);    
             */
-            // Call generic Action Method
-            DmiActions
-                .Perform_the_following_procedure_Press_Special_button_Press_Adhesion_button_Select_and_confirm_Non_slippery_rail_button(this);
-
-
+            
             /*
             Test Step 6
             Action: End of test
             Expected Result: 
             */
-
 
             return GlobalTestResult;
         }
