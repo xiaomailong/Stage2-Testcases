96cc07d69f15082e3278e5594f3f6d7a38f3e5d6
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-14 13:44:22 +0100

    Added more xml files

---
+++ Testcase/XML Packets/15/XML_15_6_2_j.cs
@@ -0,0 +1,48 @@
+﻿#region usings
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BT_Tools;
+using BT_CSB_Tools;
+using BT_CSB_Tools.Logging;
+using BT_CSB_Tools.Utils.Xml;
+using BT_CSB_Tools.SignalPoolGenerator.Signals;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+using Testcase.DMITestCases;
+
+#endregion
+
+namespace Testcase.XML
+{
+    /// <summary>
+    /// Values of 15.6.2_j.xml file
+    /// </summary>
+    static class XML_15_6_2_j
+    {
+        private static SignalPool _pool;
+
+        public static void Send(SignalPool pool)
+        {
+            _pool = pool;
+
+            
+
+            _pool.Wait_Realtime(2000);
+
+            // Send
+            EVC33_MMIAdditionalOrder.MMI_M_TRACKCOND_TYPE = Variables.MMI_M_TRACKCOND_TYPE.Level_Crossing;     // Level crossing
+            EVC33_MMIAdditionalOrder.MMI_NID_TRACKCOND = 0;
+            EVC33_MMIAdditionalOrder.MMI_Q_TRACKCOND_ACTION = MMI_Q_TRACKCOND_ACTION.WithDriverAction;
+            EVC33_MMIAdditionalOrder.MMI_Q_TRACKCOND_STEP = 1;
+            EVC33_MMIAdditionalOrder.Send();
+        }
+    }
+}
