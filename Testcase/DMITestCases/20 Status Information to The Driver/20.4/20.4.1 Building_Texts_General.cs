e325236cc25cf1e7eab0d6e57254957fcd7f8c4f
Simon Beesley <sasbeesley@gmail.com>
2017-08-29 10:05:20 +0100

    test section 20.4 with cs files for xml

--- Testcase/DMITestCases/20 Status Information to The Driver/20.4/20.4.1 Building_Texts_General.cs
+++ Testcase/DMITestCases/20 Status Information to The Driver/20.4/20.4.1 Building_Texts_General.cs
@@ -13,6 +13,8 @@ using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
 using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+
 
 namespace Testcase.DMITestCases
 {
@@ -37,16 +39,19 @@ namespace Testcase.DMITestCases
         public override void PreExecution()
         {
             // Pre-conditions from TestSpec:
-            // System is power on.Cabin is activateSoM is perform in SR mode, Level 1.The active language is English.
 
             // Call the TestCaseBase PreExecution
             base.PreExecution();
+            // System is power on.Cabin is activateSoM is perform in SR mode, Level 1.The active language is English.
+            DmiActions.Complete_SoM_L1_SR(this);
         }
 
         public override void PostExecution()
         {
             // Post-conditions from TestSpec
             // DMI displays in SR mode, Level 1.
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays in SR mode, Level 1.");
 
             // Call the TestCaseBase PostExecution
             base.PostExecution();
@@ -56,16 +61,27 @@ namespace Testcase.DMITestCases
         {
             // Testcase entrypoint
 
-
             /*
             Test Step 1
             Action: Drive the train forward pass BG1.Then, stop the train
             Expected Result: Verify the following information,Use the log file to confirm that DMI received packet EVC-8 with MMI_Q_TEXT = 256 (Plain Text Message).The plain text message ‘TEST 15.4’ is displayed on sub-area E5.Text messsage is display in white colour.The local time of appearance is attached in front of plain text messge at the first line.The local time is separated from the first character of displayed text by an indent.The format of local time is “hh:mm” with a 24 hours time reference
             Test Step Comment: (1) MMI_gen 7025 (partly: packet EVC-8, 1st bullet);(2) MMI_gen 7025 (partly: 2nd bullet, #1);(3) MMI_gen 1046 (partly: bullet a, non-acknowledgeable Text message); (4) MMI_gen 1046 (b) (partly: attached to the first line);   (5) MMI_gen 1046 (b) (partly: separated from the first character by an indent);           (6) MMI_gen 1046 (b) (partly: format of local time);
             */
-            // Call generic Action Method
-            DmiActions.Drive_the_train_forward_pass_BG1_Then_stop_the_train(this);
+            // Don't understand this part of the test!
+            EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 5;
+            EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 0;
 
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CLASS = MMI_Q_TEXT_CLASS.ImportantInformation;
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 1;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 256;
+            EVC8_MMIDriverMessage.PlainTextMessage = "TEST 15.4";
+
+            // spec says 'indent' is separator ??
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. The plain text message ‘TEST 15.4’ is displayed in white on sub-area E5." + Environment.NewLine +
+                                "2. The local time of arrival of the message is displayed on the first line in front of the plain text message" + Environment.NewLine +
+                                "3. The local time is separated from the text message by a space" + Environment.NewLine +
+                                "4. The local time format is ‘hh:mm’ with hh in range 0..23");
 
             /*
             Test Step 2
@@ -73,7 +89,7 @@ namespace Testcase.DMITestCases
             Expected Result: Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 23 (Ack of Plain Text message)
             Test Step Comment: MMI_gen 11470 (partly: Bit #23);
             */
-
+            DmiActions.ShowInstruction(this, @"Acknowledge the plain test message and check the log file for packet EVC-152 from DMI with MMI_M_DRIVER_ACTION  = 23");
 
             /*
             Test Step 3
@@ -81,7 +97,10 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,Only “ DMI” from the second packet is displayed in area E5 due to the expiry of 10 seconds
             Test Step Comment: (1) MMI_gen 7045; 
             */
-
+            XML.XML_15_4_a.Send(this);
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. The plain text message ‘TEST’ does not appear." + Environment.NewLine +
+                                "2. ‘ DMI’ is displayed in sub-area E5.");
 
             /*
             Test Step 4
@@ -89,7 +108,10 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display in sub-area E5-E9 as follows,Once the first message is sent, DMI does not show the message as the message is marked as an incomplete sentence.DMI displays “TEST DMI” in area E5 after the second message is sent
             Test Step Comment: (1) MMI_gen 2557 (partly: first part, EVC-8.criteria = 5, incomplete sentence)(2) MMI_gen 2557 (partly: 9 seconds, second part, the same index, non-ack);           MMI_gen 7046 (partly: concatenate, second packet, non-ack);      MMI_gen 7025 (partly: 2nd bullet, #1);
             */
-
+            XML.XML_15_4_b.Send(this);
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. The plain text message ‘TEST’ does not appear." + Environment.NewLine +
+                                "2. ‘TEST DMI’ is displayed in sub-area E5.");
 
             /*
             Test Step 5
@@ -97,7 +119,9 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,(1) Only “ DMI” from the second packet is displayed in area E5 due to the expiry of 11 seconds
             Test Step Comment: (1) MMI_gen 2557 (partly: 11 seconds, second part, the same index, non-ack); MMI_gen 7045; 
             */
-
+            XML.XML_15_4_i.Send(this);
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. ‘ DMI’ is displayed in sub-area E5.");
 
             /*
             Test Step 6
@@ -105,7 +129,10 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,Once the first message is sent, DMI does not show the message as the message is marked as an incomplete sentence.Once the second message is sent, DMI does not show the message as the message is marked as an incomplete sentence.DMI displays “ DMITEST” in area E5 after the third message is sent.The first message is overwritten by the second message
             Test Step Comment: (1) MMI_gen 2557 (partly: first part, EVC-8.criteria = 5, incomplete sentence);(2) MMI_gen 2557 (partly: EVC-8.criteria = 5, incomplete sentence);(3) MMI_gen 2557 (partly: 9 seconds, the same index, non-ack);           MMI_gen 7046 (partly: concatenate, non-ack);      MMI_gen 7025 (partly: 2nd bullet, #1);(4) MMI_gen 7046 (partly: overwritten);
             */
-
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. The plain text message ‘TEST’ does not appear." + Environment.NewLine +
+                                "2. The plain text message ‘ DMI’ does not appear." + Environment.NewLine +
+                                "2. ‘ DMITEST’ is displayed in sub-area E5.");
 
             /*
             Test Step 7
@@ -113,7 +140,10 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,“ DMI” from the second packet is displayed in area E5 with yellow flashing frame due to the different ID from the first packet.DMI displays “TEST DMI” in area E5 with yellow flashing frame after the third message is sent due to the same message ID as the first incomplete message
             Test Step Comment: (1) MMI_gen 2557 (partly: second part, different index);(2) MMI_gen 2557 (partly: 9 seconds, the same index, ack);           MMI_gen 7046 (partly: concatenate, ack);      MMI_gen 7025 (partly: 2nd bullet, #1);
             */
-
+            XML.XML_15_4_k.Send(this);
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. After 4s DMI displays ‘ DMI’ in area E5 with a yellow flashing frame." + Environment.NewLine +
+                                "2. After a further 4s DMI displays ‘TEST DMI’ in area E5 with a yellow flashing frame.");
 
             /*
             Test Step 8
@@ -121,7 +151,16 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display in sub-area E5-E9 as follows,The 2 plain text messages are concatenated and displayed as “ABCDEFGHIJ BCDEFGHIJ XXXXXXXXX.Text messsage is displayed in white colour.The text message is displayed as two lines in both of sub area E5 and E6.The message in sub area E6 is aligned with the with character of display text in sub area E5
             Test Step Comment: (1) MMI_gen 2557 (partly: stored first part, same-index second part, 1st bullet);           MMI_gen 7046 (partly: concatenate, 1st bullet);      (2) MMI_gen 1046 (a) (partly: building text);  (3) MMI_gen 1046 (c) (partly: building text, continue inside the next Sub-area);(4) MMI_gen 1046 (c)  (partly: building text, Next line aligned with the first character of the display text);
             */
+            EVC2_MMIStatus.MMI_M_ACTIVE_CABIN = Variables.MMI_M_ACTIVE_CABIN.NoCabinActive;
+            EVC2_MMIStatus.Send();
 
+            EVC2_MMIStatus.MMI_M_ACTIVE_CABIN = Variables.MMI_M_ACTIVE_CABIN.Cabin1Active;
+            EVC2_MMIStatus.Send();
+
+            XML.XML_15_4_c.Send(this);
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays ‘ABCDEFGHIJ BCDEFGHIJ XXXXXXXXX’ in white in sub-areas E5 and E6." + Environment.NewLine +
+                                "2. The text message is displayed in two lines in both sub-areas E5 and E6.");
 
             /*
             Test Step 9
@@ -129,7 +168,7 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display message in sub-area E5-E9 according to the order of MMI_Q_TEXT in the Action column,TPWS FailedConfirm change of inhibit STM TPWSShunting rejected due to TPWS TripTPWS needs dataTPWS brake demandTPWS is not availableNote: Use the <Up> and <Down> button to scroll the list of message in sub-area E5-E9
             Test Step Comment: MMI_gen 7025 (partly:1st bullet, 2nd bullet, #2); MMI_gen 9520 (partly: system status message in table 76 for NTC);
             */
-
+            XML.XML_15_4_d.Send(this);
 
             /*
             Test Step 10
@@ -137,7 +176,7 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display message in sub-area E5-E9 according to the order of MMI_Q_TEXT in the Action column,<Unknown>FailedConfirm change of inhibit STM <Unknown>Shunting rejected due to <Unknown>Trip<Unknown> needs data<Unknown> brake demand<Unknown> is not availableNote: Use the <Up> and <Down> button to scroll the list of message in sub-area E5-E9
             Test Step Comment: MMI_gen 7025 (partly:1st bullet, 2nd bullet, #2, unknown); MMI_gen 9520 (partly: system status message in table 76 for NTC);
             */
-
+            XML.XML_15_4_e.Send(this);
 
             /*
             Test Step 11
@@ -145,7 +184,7 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display message in sub-area E5-E9 according to the order of MMI_Q_TEXT in the Action column,Restriction 1 km/h in Release Speed Area Brake Test timeout in 1 HoursBTM Test Timeout in 1 hoursATP Restart required in 1 HoursConfirm change of inhibit Level 1Cabin Reactivation required in 1 hoursNew power-up required in 1 hoursNote: Use the <Up> and <Down> button to scroll the list of message in sub-area E5-E9
             Test Step Comment: MMI_gen 7025 (partly:1st bullet, 2nd bullet, #1);
             */
-
+            XML.XML_15_4_f.Send(this);
 
             /*
             Test Step 12
@@ -153,7 +192,7 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the display message in sub-area E5-E9 according to the order of MMI_Q_TEXT in the Action column,Restriction  km/h in Release Speed Area Brake Test timeout in  HoursBTM Test Timeout in  hoursATP Restart required in  HoursConfirm change of inhibit Level Cabin Reactivation required in  hoursNew power-up required in  hoursNote: Use the <Up> and <Down> button to scroll the list of message in sub-area E5-E9. is a solid rectangular symbol in white colour
             Test Step Comment: MMI_gen 7025 (partly:1st bullet, 2nd bullet, #1, unknown);
             */
-
+            XML.XML_15_4_g.Send(this);
 
             /*
             Test Step 13
@@ -161,14 +200,13 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,DMI displays driver message ‘BTM Test Timeout’ in sub-area E5. (MMI_X_TEXT is ignored)DMI displays symbol ST02 symbol  in sub-area C9. (MMI_X_TEXT is ignored)
             Test Step Comment: (1) MMI_gen 1066 (partly: #1, #2);(2) MMI_gen 1066 (partly: #4, ST02);
             */
-
+            XML.XML_15_4_h.Send(this);
 
             /*
             Test Step 14
             Action: End of test
             Expected Result: 
             */
-
 
             return GlobalTestResult;
         }
---
+++ Testcase/XML Packets/15/XML_15_4_f.cs
@@ -0,0 +1,108 @@
+﻿#region usings
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BT_Tools;
+using BT_CSB_Tools;
+using BT_CSB_Tools.Logging;
+using BT_CSB_Tools.Utils.Xml;
+using BT_CSB_Tools.SignalPoolGenerator.Signals;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+using Testcase.DMITestCases;
+
+#endregion
+
+
+namespace Testcase.XML
+{
+    /// <summary>
+    /// Values of 15.4.f.xml file
+    /// </summary>
+    static class XML_15_4_f
+    {
+        private static SignalPool _pool;
+
+        public static void Send(SignalPool pool)
+        {
+            _pool = pool;
+                        
+            // Step 11/1
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CLASS = MMI_Q_TEXT_CLASS.ImportantInformation;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 3;
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 1;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 522;
+            EVC8_MMIDriverMessage.PlainTextMessage = "1";
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                      @"1. DMI displays ‘Restriction 1 km/h in Release Speed Area’ in areas E5-E9.");           
+
+            // Step 11/2         
+            // spec says I_TEXT = 1 (fixed) but xml 'increments' in each message
+            // assume xml is correct   
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 2;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 525;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘Brake Test timeout in 1 Hours’ in areas E5-E9.");
+
+            // Step 11/3
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 3;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 534;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘BTM Test Timeout in 1 hours’ in areas E5-E9.");
+
+            // Step 11/4
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 4;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 535;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘ATP Restart required in 1 Hours’ in areas E5-E9.");
+
+            // Step 11/5
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 5;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 564;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘Confirm change of inhibit Level 1’ in areas E5-E9.");
+
+            // Step 11/6
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 6;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 574;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘Cabin Reactivation required in 1 hours’ in areas E5-E9."); 
+            
+            // Step 11/7
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 7;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 705;
+            EVC8_MMIDriverMessage.Send();
+
+            _pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                       @"1. DMI displays ‘New power-up required in 1 hours’ in areas E5-E9.");
+
+            _pool.WaitForVerification(@"Using the <Up> and <Down> buttons, check that the order of all messages top to bottom is:" + Environment.NewLine + Environment.NewLine +
+                                      @"1. DMI displays ‘New power-up required in 1 hours’ in areas E5-E9." + Environment.NewLine +
+                                      @"2. DMI displays Cabin Reactivation required in 1 hours’ in areas E5-E9." + Environment.NewLine +
+                                      @"3. DMI displays ‘Confirm change of inhibit Level 1’ in areas E5-E9." + Environment.NewLine +
+                                      @"4. DMI displays ‘ATP Restart required in 1 Hours’ in areas E5-E9." + Environment.NewLine +
+                                      @"5. DMI displays ‘BTM Test Timeout in 1 hours’ in areas E5-E9." + Environment.NewLine +
+                                      @"6. DMI displays ‘Brake Test timeout in 1 Hours’ in areas E5 - E9." + Environment.NewLine +
+                                      @"7. DMI displays ‘Restriction 1 km/h in Release Speed Area’ in areas E5 - E9.");
+        }
+    }
+}
