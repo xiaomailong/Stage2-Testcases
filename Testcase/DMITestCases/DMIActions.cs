1411efa19071c058e983689f03cc1d45b4c40a25
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-30 13:55:24 +0100

    Updated Send_EVC20_MMISelectLevel_AllLevels() to reflect enum changes

--- Testcase/DMITestCases/DMIActions.cs
+++ Testcase/DMITestCases/DMIActions.cs
@@ -168,12 +168,12 @@ namespace Testcase.DMITestCases
                 MMI_Q_LEVEL_NTC_ID.STM_ID,
                 MMI_Q_LEVEL_NTC_ID.STM_ID };
             Variables.MMI_M_CURRENT_LEVEL[] paramEvc20MmiMCurrentLevel =
-                { MMI_M_CURRENT_LEVEL.NotLatestUsedLevel,
-                MMI_M_CURRENT_LEVEL.NotLatestUsedLevel,
-                MMI_M_CURRENT_LEVEL.NotLatestUsedLevel,
-                MMI_M_CURRENT_LEVEL.NotLatestUsedLevel,
-                MMI_M_CURRENT_LEVEL.NotLatestUsedLevel,
-                MMI_M_CURRENT_LEVEL.NotLatestUsedLevel };
+                { MMI_M_CURRENT_LEVEL.NotLastUsedLevel,
+                MMI_M_CURRENT_LEVEL.NotLastUsedLevel,
+                MMI_M_CURRENT_LEVEL.NotLastUsedLevel,
+                MMI_M_CURRENT_LEVEL.NotLastUsedLevel,
+                MMI_M_CURRENT_LEVEL.NotLastUsedLevel,
+                MMI_M_CURRENT_LEVEL.NotLastUsedLevel };
             Variables.MMI_M_LEVEL_FLAG[] paramEvc20MmiMLevelFlag =
                 { MMI_M_LEVEL_FLAG.MarkedLevel,
                 MMI_M_LEVEL_FLAG.MarkedLevel,
@@ -229,13 +229,19 @@ namespace Testcase.DMITestCases
         }
 
         /// <summary>
-        /// Description: Prompts the tester with a dialog box
+        /// Prompts the tester with a dialog box showing a set of instructions.
         /// </summary>
         public static void ShowInstruction(SignalPool pool, string instruction)
         {
             pool.WaitForAcknowledgement(instruction);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="caption"></param>
+        /// <returns></returns>
         public static string ShowDialog(string text, string caption)
         {
             Form prompt = new Form()
@@ -246,9 +252,11 @@ namespace Testcase.DMITestCases
                 Text = caption,
                 StartPosition = FormStartPosition.CenterScreen
             };
+
             Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
             TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
             Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+
             confirmation.Click += (sender, e) => { prompt.Close(); };
             prompt.Controls.Add(textBox);
             prompt.Controls.Add(confirmation);
