37f13233c059edbf92d1b27c9a2e237ca7e176b1
Robert van Vaardegem <robert.van.v@gmail.com>
2017-08-30 13:54:48 +0100

    Added class summary comments

--- Testcase/Telegrams/EVCtoDMI/EVC20_MMISelectLevel.cs
+++ Testcase/Telegrams/EVCtoDMI/EVC20_MMISelectLevel.cs
@@ -5,6 +5,14 @@ using static Testcase.Telegrams.EVCtoDMI.Variables;
 
 namespace Testcase.Telegrams.EVCtoDMI
 {
+    /// <summary>
+    /// This packet shall be sent when the ETC requests the driver to select level.
+    /// The packet contains a list of ETCS and NTC levels and related additional status information.
+    /// Possible use cases are:
+    ///     display of 'default level list',
+    ///     display of 'trackside supported level list',
+    ///     display of 'inhibit level list'.
+    /// </summary>
     public static class EVC20_MMISelectLevel
     {
         private static SignalPool _pool;
@@ -15,7 +23,7 @@ namespace Testcase.Telegrams.EVCtoDMI
         private static MMI_M_INHIBIT_ENABLE[] _mInhibitEnable;
         private static MMI_M_LEVEL_NTC_ID[] _mLevelNtcId;
         private static bool _qCloseEnable;
-        private static int _nLevels;
+        private static ushort _nLevels;
 
         /// <summary>
         /// Initialise an instance of EVC-20 MMI Select Level telegram.
@@ -42,49 +50,52 @@ namespace Testcase.Telegrams.EVCtoDMI
                   Equals(_mInhibitEnable.Length, _mLevelNtcId.Length) &&
                   Equals(_mLevelNtcId.Length, _mCurrentLevel.Length)))
             {
-                throw new Exception("Number of Level does not match!");
+                throw new Exception("Array lengths do not match!");
             }
 
-            // determine how many level will be sent
+            // Determine how many levels will be sent
             _nLevels = (ushort) _qLevelNtcId.Length;
-            // fill telegram with MMI_N_LEVELS
-            _pool.SITR.ETCS1.SelectLevel.MmiNLevels.Value = (ushort) _nLevels;
+            
+            // Populate telegram with MMI_N_LEVELS
+            _pool.SITR.ETCS1.SelectLevel.MmiNLevels.Value = _nLevels;
 
             for (var k = 0; k < _nLevels; k++)
             {
                 // xxxx xxxx => x000 0000
                 uint uintMmiQLevelNtcId = Convert.ToUInt32(_qLevelNtcId[k]);
                 uintMmiQLevelNtcId <<= 7;
+                
                 // xxxx xxxx => 0x00 0000
                 uint uintMmiMCurrentLevel = Convert.ToUInt32(_mCurrentLevel[k]);
                 uintMmiMCurrentLevel <<= 6;
+                
                 // xxxx xxxx => 00x0 0000
                 uint uintMmiMLevelFlag = Convert.ToUInt32(_mLevelFlag[k]);
                 uintMmiMLevelFlag <<= 5;
+                
                 // xxxx xxxx => 000x 0000
                 uint uintMmiMInhibitedLevel = Convert.ToUInt32(_mInhibitedLevel[k]);
                 uintMmiMInhibitedLevel <<= 4;
+                
                 // xxxx xxxx => 0000 x000
                 uint uintMmiMInhibitEnable = Convert.ToUInt32(_mInhibitEnable[k]);
                 uintMmiMInhibitEnable <<= 3;
 
-                // build EVC20_alias_1 
+                // Build EVC20_alias_1 
                 byte evc20Alias1 = Convert.ToByte(uintMmiQLevelNtcId | uintMmiMCurrentLevel |
                                                   uintMmiMLevelFlag | uintMmiMInhibitedLevel |
                                                   uintMmiMInhibitEnable);
 
-                // fill telegram with dynamic fields
+                // Populate telegram with dynamic fields
                 if (k < 10)
                 {
-                    _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub0" + k + "_EVC20alias1",
-                        evc20Alias1);
+                    _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub0" + k + "_EVC20alias1", evc20Alias1);
                     _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub0" + k + "_MmiMLevelNtcId",
                         Convert.ToByte(_mLevelNtcId[k]));
                 }
                 else
                 {
-                    _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub" + k + "_EVC20alias1",
-                        evc20Alias1);
+                    _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub" + k + "_EVC20alias1", evc20Alias1);
                     _pool.SITR.Client.Write("ETCS1_SelectLevel_EVC20SelectLevelSub" + k + "_MmiMLevelNtcId",
                         Convert.ToByte(_mLevelNtcId[k]));
                 }
@@ -103,6 +114,7 @@ namespace Testcase.Telegrams.EVCtoDMI
 
         /// <summary>
         /// Qualifier for the variable MMI_M_LEVEL_NTC_ID
+        /// 
         /// Values:
         /// 0 = "MMI_M_LEVEL_NTC_ID contains an STM ID (0-255)"
         /// 1 = "MMI_M_LEVEL_NTC_ID contains a level number (0-3)"
@@ -114,9 +126,10 @@ namespace Testcase.Telegrams.EVCtoDMI
 
         /// <summary>
         /// Indicates if MMI_M_LEVEL_STM_ID is the latest used level
+        /// 
         /// Values:
         /// 0 = "MMI_M_LEVEL_STM_ID is not the latest used level"
-        /// 1 ="MMI_M_LEVEL_STM_ID is the latest used level"
+        /// 1 = "MMI_M_LEVEL_STM_ID is the latest used level"
         /// </summary>
         public static MMI_M_CURRENT_LEVEL[] MMI_M_CURRENT_LEVEL
         {
@@ -124,8 +137,11 @@ namespace Testcase.Telegrams.EVCtoDMI
         }
 
         /// <summary>
-        /// Indicates if MMI_M_LEVEL_NTC_ID is marked or not. The interpretation of the mark needs to be defined by related requirements. 
-        /// Basic idea is that 'marked' levels are allowed for edit by the driver (see ERA_ERTMS_15560, v.3.4.9, ch. 11.3.2.7, 11.3..2.8)
+        /// Indicates if MMI_M_LEVEL_NTC_ID is marked or not.
+        /// The interpretation of the mark needs to be defined by related requirements. 
+        /// Basic idea is that 'marked' levels are allowed for edit by the driver
+        /// (see ERA_ERTMS_15560, v.3.4.9, ch. 11.3.2.7, 11.3..2.8)
+        /// 
         /// Values:
         /// 0 = "MMI_M_LEVEL_NTC_ID is 'not marked'"
         /// 1 = "MMI_M_LEVEL_NTC_ID is 'marked'"
@@ -136,7 +152,8 @@ namespace Testcase.Telegrams.EVCtoDMI
         }
 
         /// <summary>
-        /// Indicates if MMI_M_LEVEL_NTC_ID is currently inhibited by driver or not 
+        /// Indicates if MMI_M_LEVEL_NTC_ID is currently inhibited by driver or not.
+        /// 
         /// Values:
         /// 0 = "MMI_M_LEVEL_NTC_ID is not inhibited"
         /// 1 = "MMI_M_LEVEL_NTC_ID is inhibited"
@@ -147,7 +164,8 @@ namespace Testcase.Telegrams.EVCtoDMI
         }
 
         /// <summary>
-        /// Indicates if MMI_M_LEVEL_NTC_ID is allowed (configurable) for inhibiting or not
+        /// Indicates if MMI_M_LEVEL_NTC_ID is allowed (configurable) for inhibiting or not.
+        /// 
         /// Values:
         /// 0 = "MMI_M_LEVEL_NTC_ID is not allowed for inhibiting"
         /// 1 = "MMI_M_LEVEL_NTC_ID is allowed for inhibiting"
@@ -159,6 +177,7 @@ namespace Testcase.Telegrams.EVCtoDMI
 
         /// <summary>
         /// Identity of level or NTC
+        /// 
         /// Note: If MMI_Q_LEVEL_NTC_ID is 0 the value means NTC Identity 0-255
         /// If MMI_Q_LEVEL_NTC_ID is 1 the value means Level 0-3
         /// </summary>
@@ -181,10 +200,13 @@ namespace Testcase.Telegrams.EVCtoDMI
             set
             {
                 _qCloseEnable = value;
+                
                 // Convert to byte
                 byte _mmiQCloseEnable = Convert.ToByte(_qCloseEnable);
+               
                 // 0000 000x => x000 0000
                 _mmiQCloseEnable <<= 7;
+               
                 // Fill telegram with MMI_Q_CLOSE_ENABLE
                 _pool.SITR.ETCS1.SelectLevel.MmiQCloseEnable.Value = _mmiQCloseEnable;               
             }
