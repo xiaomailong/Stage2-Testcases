5735c63c79ead6efcc050230b9010a7e209128c7
Sam Sivahary <ext.samson.sivahary@rail.bombardier.com>
2017-09-14 16:43:17 +0200

    TC 15.1.6 updated

--- Testcase/DMITestCases/20 Status Information to The Driver/20.1/20.1.6 Mode_Symbols_for_LS_mode.cs
+++ Testcase/DMITestCases/20 Status Information to The Driver/20.1/20.1.6 Mode_Symbols_for_LS_mode.cs
@@ -61,46 +61,126 @@ namespace Testcase.DMITestCases
             /*
             Test Step 1
             Action: Drive the train forward passing BG1
-            Expected Result: Verify the following information,The symbol MO22 is displayed for Limited Supervision acknowledegement in sub-area C1.Use the log file to confirm that DMI receives packet information EVC-8 with the following value,MMI_Q_TEXT = 709MMI_Q_TEXT_CRITERIA = 1
-            Test Step Comment: (1) MMI_gen 1227 (partly: MO22);                                          (2) MMI_gen 11233 (partly: MO22);
+            Expected Result: Verify the following information,
+            The symbol MO22 is displayed for Limited Supervision acknowledegement in sub-area C1.
+            Use the log file to confirm that DMI receives packet information EVC-8 with the following value,
+            MMI_Q_TEXT = 709
+            MMI_Q_TEXT_CRITERIA = 1
+            Test Step Comment: (1) MMI_gen 1227 (partly: MO22);                                          
+                               (2) MMI_gen 11233 (partly: MO22);
             */
 
             DmiActions.Drive_train_forward_passing_BG1(this);
 
             DmiActions.Send_LS_Mode_Ack(this);
+            DmiExpectedResults.LS_Mode_Ack_Requested(this);
 
             #endregion
 
+            #region Test Step 2
             /*
             Test Step 2
             Action: Press an acknowledgement of LS mode in sub-area C1 and stop the train
-            Expected Result: DMI displays in LS mode, Level 1.Verify the following information,Use the log file to verify that DMI received the EVC-7 with [MMI_ETCS_MISC_OUT_SIGNALS.OBU_TR_M_MODE] = 12 in order to display the Limited Supervision symbol.The Limited Supervision symbol (MO21) is displayed in area B7.The symbol LS01 is displayed in sub-area A1.The number of LSSMA is displayed vertically and horizontally centered in sub-area A1.The number of LSSMA is overlay the LS01 symbol.The colour of LSSMA is grey.Use the log file to confirm that DMI received packet EVC-23 with variable MMI_V_LSSMA and the value of variable is same as displayed information in sub-area A1. Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 13 (Ack of Limited Supervision mode)
-            Test Step Comment: (1) MMI_gen 11084 (partly: current ETCS mode);                                 (2) MMI_gen 110 (partly: MO21);(3) MMI_gen 9441;(4) MMI_gen 9436; MMI_gen 9440 (partly: LSSMA shall be shown with a number, vertically and horizontally centered in A1);(5) MMI_gen 9442; MMI_gen 10489 (partly: LS mode);(6) MMI_gen 9440 (partly: grey);(7) MMI_gen 9440 (partly: packet EVC-23);(8) MMI_gen 11470 (partly: Bit # 13);
+            Expected Result: DMI displays in LS mode, Level 1.
+            Verify the following information,Use the log file to verify that 
+            DMI received the EVC-7 with [MMI_ETCS_MISC_OUT_SIGNALS.OBU_TR_M_MODE] = 12 in order to display the Limited Supervision symbol.
+            The Limited Supervision symbol (MO21) is displayed in area B7.
+            The symbol LS01 is displayed in sub-area A1.
+            The number of LSSMA is displayed vertically and horizontally centered in sub-area A1.
+            The number of LSSMA is overlay the LS01 symbol.
+            The colour of LSSMA is grey.
+            Use the log file to confirm that DMI received packet EVC-23 
+            with variable MMI_V_LSSMA and the value of variable is same as displayed information in sub-area A1. 
+            Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] 
+            with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,
+            a)   MMI_M_DRIVER_ACTION = 13 (Ack of Limited Supervision mode)
+            Test Step Comment: (1) MMI_gen 11084 (partly: current ETCS mode);
+                               (2) MMI_gen 110 (partly: MO21);
+                               (3) MMI_gen 9441;
+                               (4) MMI_gen 9436; MMI_gen 9440 (partly: LSSMA shall be shown with a number, vertically and horizontally centered in A1);
+                               (5) MMI_gen 9442; MMI_gen 10489 (partly: LS mode);
+                               (6) MMI_gen 9440 (partly: grey);
+                               (7) MMI_gen 9440 (partly: packet EVC-23);
+                               (8) MMI_gen 11470 (partly: Bit # 13);
             */
 
+            DmiActions.ShowInstruction(this, @"Perform the following actions on the DMI within 3 seconds after pressing 'OK': " + Environment.NewLine + Environment.NewLine +
+                                "1. Press DMI Sub Area C1." + Environment.NewLine);
+            DmiExpectedResults.LS_Mode_Ack_pressed_and_released(this);
 
+            DmiActions.Send_LS_Mode(this);
+            DmiExpectedResults.LS_Mode_displayed(this);
+
+            DmiActions.Stop_the_train(this);
+
+            DmiActions.Send_LSSMA(this);
+            DmiExpectedResults.LSSMA_displayed(this);
+
+            #endregion
+
+            #region Test Step 3
             /*
             Test Step 3
-            Action: Drive the train forward pass the position 250m.Then, stop the train
-            Expected Result: DMI displays in FS mode, Level 1.Verify the following information,The LSSMA is not displayed in sub-area A1
+            Action: Drive the train forward pass the position 250m.
+            Then, stop the train
+            Expected Result: DMI displays in FS mode, Level 1.
+            Verify the following information,The LSSMA is not displayed in sub-area A1
             Test Step Comment: (1) MMI_gen 10489 (partly: FS);
             */
 
+            DmiActions.Drive_the_train_forward(this);
 
+            Wait_Realtime(10000);
+
+            DmiActions.Send_FS_Mode(this);
+            DmiExpectedResults.FS_mode_displayed(this);
+
+            DmiActions.Stop_the_train(this);
+
+            //DmiActions.ShowInstruction(this, "EVC shall send 65536 for LSSMA value.");
+            //DmiActions.Send_LSSMA(this);
+            WaitForVerification("Has the LSSMA (LS01) been removed from sub-area A1 on DMI?");
+
+            #endregion
+
+            #region Test Step 4
             /*
             Test Step 4
-            Action: Drive the train forward pass BG2.Then, press an acknowledgement of OS mode in sub-area C1
-            Expected Result: DMI displays in OS mode, Level 1.Verify the following information,Use the log file to confirm that DMI received packet EVC-23 with variable MMI_V_LSSMA and use it to display on sub-area A1.Note: The LSSMA is sub-area A1 will not display if MMI_V_LSSMA = 65535 (special value)
-            Test Step Comment: (1) MMI_gen 9971; MMI_gen 10489 (partly: display, OS mode);
+            Action: Drive the train forward pass BG2.
+            Then, press an acknowledgement of OS mode in sub-area C1
+            Expected Result: DMI displays in OS mode, Level 1.
+            Verify the following information,
+            Use the log file to confirm that DMI received packet EVC-23 
+            with variable MMI_V_LSSMA and use it to display on sub-area A1.
+            Note: The LSSMA is sub-area A1 will not display if MMI_V_LSSMA = 65535 (special value)
+            Test Step Comment: (1) MMI_gen 9971; 
+                                   MMI_gen 10489 (partly: display, OS mode);
             */
 
+            DmiActions.Drive_train_forward_passing_BG2(this);
 
+            DmiActions.Send_OS_Mode_Ack(this);
+            DmiExpectedResults.OS_Mode_Ack_Requested(this);
+
+            DmiActions.ShowInstruction(this, @"Perform the following actions on the DMI within 3 seconds after pression 'OK': " + Environment.NewLine + Environment.NewLine +
+                               "1. Press DMI Sub Area C1." + Environment.NewLine);
+            DmiExpectedResults.OS_Mode_Ack_pressed_and_released(this);
+
+            DmiActions.Send_OS_Mode(this);
+            DmiExpectedResults.OS_Mode_displayed(this);
+
+            DmiActions.ShowInstruction(this, "EVC shall send 65536 for LSSMA value.");
+            DmiActions.Send_LSSMA(this);
+
+            #endregion
+
+            #region Test Step 5
             /*
             Test Step 5
             Action: End of test
             Expected Result: 
             */
-
+            #endregion
 
             return GlobalTestResult;
         }
