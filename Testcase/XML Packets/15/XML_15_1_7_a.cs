855d8aaba14ee558bc2aeec4eb44b4468f323567
Sam Sivahary <ext.samson.sivahary@rail.bombardier.com>
2017-09-15 10:01:17 +0200

    XML class created for 15.1.7

---
+++ Testcase/XML Packets/15/XML_15_1_7_a.cs
@@ -0,0 +1,47 @@
+﻿#region usings
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BT_Tools;
+using BT_CSB_Tools;
+using BT_CSB_Tools.Logging;
+using BT_CSB_Tools.Utils.Xml;
+using BT_CSB_Tools.SignalPoolGenerator.Signals;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+
+#endregion
+
+namespace Testcase.XML
+{
+    /// <summary>
+    /// Values of 15.1.7.a.xml file
+    /// </summary>
+    static class XML_15_1_7_a
+    {
+        private static SignalPool _pool;
+
+        public static void Send(SignalPool pool)
+        {
+            _pool = pool;
+
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CLASS = MMI_Q_TEXT_CLASS.ImportantInformation;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 1;
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 1;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT = 674;
+            EVC8_MMIDriverMessage.Send();
+
+            pool.WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                     "1. The text message ‘Fixed Text Message 674’ is displayed in area E5." + Environment.NewLine +
+                                     "2. No flashing frame is displayed" + Environment.NewLine +
+                                     "3. No sound is played.");
+        }
+    }
+}
