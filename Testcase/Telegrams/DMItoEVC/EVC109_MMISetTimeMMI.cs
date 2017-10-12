89ce65190dd121705885d5a4e16eeaa58da2a425
Robert van Vaardegem <robert.van.v@gmail.com>
2017-10-12 15:44:44 +0100

    Added EVC109 for Cliff to flesh out.

---
+++ Testcase/Telegrams/DMItoEVC/EVC109_MMISetTimeMMI.cs
@@ -0,0 +1,23 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+using Testcase.DMITestCases;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+using static Testcase.Telegrams.EVCtoDMI.Variables;
+#endregion
+
+namespace Testcase.Telegrams.DMItoEVC
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class EVC109_MMISetTimeMMI
+    {
+        private static SignalPool _pool;
+    }
+}
