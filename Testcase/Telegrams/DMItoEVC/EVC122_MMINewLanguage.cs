7afb98040caa7a97777517ee8dc7ccb5dd568da9
Sam Sivahary <ext.samson.sivahary@rail.bombardier.com>
2017-10-17 15:41:26 +0200

    EVC122 packet created

---
+++ Testcase/Telegrams/DMItoEVC/EVC122_MMINewLanguage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using CL345;
+using Testcase.Telegrams.EVCtoDMI;
+using Testcase.DMITestCases;
+using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
+
+
+namespace Testcase.Telegrams.DMItoEVC
+{
+    /// <summary>
+    /// This packet will be sent when the driver has selected a new language with the language button.
+    /// </summary>
+    public static class EVC122_MMINewLanguage
+    {
+        private static SignalPool _pool;
+        private static bool _checkResult;
+        const string baseString = "DMI->ETCS: EVC-122 [MMI_NEW_LANGUAGE]";
+
+        /// <summary>
+        /// Initialise EVC-122 MMI_New_Language.
+        /// </summary>
+        /// <param name="pool"></param>
+        public static void Initialise(SignalPool pool)
+        {
+            _pool = pool;
+            _pool.SITR.SMDStat.CCUO.ETCS1NewLanguage.Value = 0x00;
+            _pool.SITR.SMDCtrl.CCUO.ETCS1NewLanguage.Value = 0x0001;
+        }
+
+        private static void CheckNidLanguage()
+        {
+            // Reset telegram received flag in RTSim
+            _pool.SITR.SMDStat.CCUO.ETCS1NewLanguage.Value = 0x00;
+
+            // Check if telegram received flag has been set.
+            if (_pool.SITR.SMDStat.CCUO.ETCS1NewLanguage.WaitForCondition(Is.Equal, 1, 20000, 100))
+            {
+                // If check passes
+                if (_pool.SITR.CCUO.ETCS1NewLanguage.MmiNidLanguage.Value.Equals(MMI_NID_LANGUAGE))
+                {
+                    _pool.TraceReport($"{baseString} - [MMI_NID_LANGUAGE] = {MMI_NID_LANGUAGE}" + Environment.NewLine +
+                                        "Result: PASSED.");
+                }
+                // Else display the real value extracted from EVC-104
+                else
+                {
+                    _pool.TraceError($"{baseString} - [MMI_NID_LANGUAGE] = " + _pool.SITR.CCUO.ETCS1NewLanguage.MmiNidLanguage.Value +
+                                    Environment.NewLine + "Result: FAILED");
+                }
+            }
+            // Show generic DMI -> EVC telegram failure
+            else
+            {
+                DmiExpectedResults.DMItoEVC_Telegram_Not_Received(_pool, baseString);
+            }
+        }
+
+        /// <summary>
+        /// Language code.
+        /// 
+        /// Values:
+        /// 255 = "'Language unknown'"
+        /// 
+        /// Note: No mapping between value and language is required for ETC, only storage and transfer of the value.
+        /// </summary>
+        public static uint MMI_NID_LANGUAGE { get; set; }
+     
+    }
+}
