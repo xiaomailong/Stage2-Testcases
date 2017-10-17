5e9d65cc67a732c45e7f1103041400d5bc806a3b
Kevin Hudson <khudson850@aol.com>
2017-10-17 13:53:47 +0100

    Add skeleton for evc28

---
+++ Testcase/Telegrams/EVCtoDMI/EVC28_MMIEchoedSetVBCData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testcase.Telegrams.EVCtoDMI
+{
+    public static class EVC28_MMIEchoedSetVBCData
+    {
+
+    }
+}
