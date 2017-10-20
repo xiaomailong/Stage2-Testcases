71250af04b3630a17ab90a7c4a1b537c0a08c016
Simon Beesley <sasbeesley@gmail.com>
2017-10-20 13:54:11 +0100

    test: 29.1, 29.2, 29.3 complete

--- Testcase/DMITestCases/29 UTC Distribution/29.2/29.2 UTC_time_and_offset_timeby_using_EVC_3.cs
+++ Testcase/DMITestCases/29 UTC Distribution/29.2/29.2 UTC_time_and_offset_timeby_using_EVC_3.cs
@@ -13,6 +13,9 @@ using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
 using CL345;
+using Testcase.Telegrams.DMItoEVC;
+using Testcase.Telegrams.EVCtoDMI;
+
 
 namespace Testcase.DMITestCases
 {
@@ -37,12 +40,13 @@ namespace Testcase.DMITestCases
     /// Used files:
     /// 29_2_a.xml, 29_2_a.xml
     /// </summary>
-    public class UTC_time_and_offset_timeby_using_EVC_3 : TestcaseBase
+    public class TC_ID_29_2_UTC_time_and_offset_timeby_using_EVC_3 : TestcaseBase
     {
         public override void PreExecution()
         {
             // Pre-conditions from TestSpec:
-            // Power off test system Set the following tags name in configuration file (See the instruction in Appendix 1)CLOCK_TIME_SOURCE = 0 (by Deafult)
+            // Power off test system Set the following tags name in configuration file (See the instruction in Appendix 1)
+            // CLOCK_TIME_SOURCE = 0 (by Deafult)
 
             // Call the TestCaseBase PreExecution
             base.PreExecution();
@@ -60,37 +64,43 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // Testcase entrypoint
-
+            TraceInfo("This test case requires an ATP configuration change - " +
+                      "See Precondition requirements. If this is not done manually, the test may fail!");
 
             /*
             Test Step 1
             Action: Power on the system and activate the cabin
             Expected Result: DMI displays SB mode
             */
-            // Call generic Action Method
-            DmiActions.Power_on_the_system_and_activate_the_cabin(this);
-            // Call generic Check Results Method
-            DmiExpectedResults.SB_Mode_displayed(this);
-
+            // tested in 29.1 so not repeated
 
             /*
             Test Step 2
             Action: Perform SoM to L1, SR mode
             Expected Result: Mode changes to SR mode , L1
             */
-            // Call generic Action Method
-            DmiActions.Perform_SoM_to_L1_SR_mode(this);
-            // Call generic Check Results Method
-            DmiExpectedResults.SR_Mode_displayed(this);
+            DmiActions.Complete_SoM_L1_SR(this);
 
+            EVC30_MMIRequestEnable.SendBlank();
+            EVC30_MMIRequestEnable.MMI_NID_WINDOW = 0;      // Default window
+            EVC30_MMIRequestEnable.MMI_Q_REQUEST_ENABLE_HIGH = EVC30_MMIRequestEnable.EnabledRequests.SetLocalTimeDateAndOffset;
+            EVC30_MMIRequestEnable.Send();
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays in SR mode, Level 1.");
+            
             /*
             Test Step 3
             Action: Use the test script file 29_2_a.xml to send EVC-3 with,MMI_T_UTC = 946728000(2000-01-01,12:00:00)MMI_T_Zone_OFFSET = 4Note: The resolution of MMI_T_Zone_OFFSET is 0.25 hour
             Expected Result: Verify the following information:DMI displays the updated time as 13:00:00
             Test Step Comment: MMI_gen 2429; MMI_gen 2421 (partly: ETC to ETCS-MMI);
             */
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
 
+            XML_29_2(msgType.msgTypeA);
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the updated time as ‘13:00:00’.");
 
             /*
             Test Step 4
@@ -98,9 +108,11 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:The set clock window is openedThe DMI (yyyy-mm-dd) date is 2000-01-01
             Test Step Comment: MMI_gen 2429; 
             */
-            // Call generic Action Method
-            DmiActions.Select_Settings_button_and_press_Set_clock_button(this);
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is ‘2000-01-01");
 
             /*
             Test Step 5
@@ -108,7 +120,12 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:DMI displays the updated time as 11:00:00
             Test Step Comment: MMI_gen 2429; MMI_gen 2421 (partly: ETC to ETCS-MMI);
             */
+            DmiActions.ShowInstruction(this, @"Close the ‘Set clock’ window");
 
+            XML_29_2(msgType.msgTypeB);
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the updated time as ‘11:00:00’.");
 
             /*
             Test Step 6
@@ -116,9 +133,11 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:The set clock window is openedThe DMI date (yyyy-mm-dd) is 2000-01-02
             Test Step Comment: MMI_gen 2429; 
             */
-            // Call generic Action Method
-            DmiActions.Select_Settings_button_and_press_Set_clock_button(this);
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is ‘2000-01-02");
 
             /*
             Test Step 7
@@ -126,8 +145,34 @@ namespace Testcase.DMITestCases
             Expected Result: 
             */
 
-
             return GlobalTestResult;
         }
+
+        #region XML_29_2_DMI_Test_Specification
+
+        private enum msgType : byte
+        {
+            msgTypeA,
+            msgTypeB
+        }
+
+        private void XML_29_2(msgType packetSelector)
+        {
+            switch (packetSelector)
+            {
+                case msgType.msgTypeA:
+                    EVC3_MMISetTimeATP.MMI_T_UTC = 946728000;
+                    EVC3_MMISetTimeATP.MMI_T_ZONE_OFFSET = 4;
+                    break;
+                case msgType.msgTypeB:
+                    EVC3_MMISetTimeATP.MMI_T_UTC = 946771200;
+                    EVC3_MMISetTimeATP.MMI_T_ZONE_OFFSET = -4;      // 252 is 0xfc - as sbyte == -0x80 + 0x7c (-128 + 124)
+                    break;
+            }   
+            
+            EVC3_MMISetTimeATP.Send();
+        }
+
+        #endregion
     }
 }
--- Testcase/DMITestCases/29 UTC Distribution/29.3/29.3 UTC_time_and_offset_timeBy_VAP_acting_as_NTP_server.cs
+++ Testcase/DMITestCases/29 UTC Distribution/29.3/29.3 UTC_time_and_offset_timeBy_VAP_acting_as_NTP_server.cs
@@ -13,6 +13,9 @@ using BT_CSB_Tools.SignalPoolGenerator.Signals.MwtSignal.Misc;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal;
 using BT_CSB_Tools.SignalPoolGenerator.Signals.PdSignal.Misc;
 using CL345;
+using Testcase.Telegrams.DMItoEVC;
+using Testcase.Telegrams.EVCtoDMI;
+
 
 namespace Testcase.DMITestCases
 {
@@ -37,12 +40,13 @@ namespace Testcase.DMITestCases
     /// Used files:
     /// 29_3_1.xml, 29_3_2.xml.
     /// </summary>
-    public class UTC_time_and_offset_timeBy_VAP_acting_as_NTP_server : TestcaseBase
+    public class TC_ID_29_3_UTC_time_and_offset_timeBy_VAP_acting_as_NTP_server : TestcaseBase
     {
         public override void PreExecution()
         {
             // Pre-conditions from TestSpec:
-            // 1.  Power off test system 2.  Set the following tags name in configuration file (See the instruction in Appendix 1)CLOCK_TIME_SOURCE =  5 (by VAP)
+            // 1.  Power off test system 2.  Set the following tags name in configuration file (See the instruction in Appendix 1)
+            // CLOCK_TIME_SOURCE =  5 (by VAP)
 
             // Call the TestCaseBase PreExecution
             base.PreExecution();
@@ -60,35 +64,41 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // Testcase entrypoint
+            TraceInfo("This test case requires an ATP configuration change - " +
+                      "See Precondition requirements. If this is not done manually, the test may fail!");
 
-
+            // tested in 29.1 so not repeated
             /*
             Test Step 1
             Action: Power on test system and activate the cabin A (MMI 1)
             Expected Result: DMI displays SB mode
             */
-            // Call generic Check Results Method
-            DmiExpectedResults.SB_Mode_displayed(this);
-
 
             /*
             Test Step 2
             Action: Perform SoM to L1, SR mode
             Expected Result: Mode changes to SR mode
             */
-            // Call generic Action Method
-            DmiActions.Perform_SoM_to_L1_SR_mode(this);
+            DmiActions.Complete_SoM_L1_SR(this);
 
+            EVC30_MMIRequestEnable.SendBlank();
+            EVC30_MMIRequestEnable.MMI_NID_WINDOW = 0;      // Default window
+            EVC30_MMIRequestEnable.MMI_Q_REQUEST_ENABLE_HIGH = EVC30_MMIRequestEnable.EnabledRequests.SetLocalTimeDateAndOffset;
+            EVC30_MMIRequestEnable.Send();
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays in SR mode, Level 1.");
+            
             /*
             Test Step 3
             Action: Use the test script file 29_3_1.xml to send packet EVC-TBD with,MMI_T_UTC = 946728000(2000-01-01,12:00:00)MMI_T_Zone_OFFSET =+5
             Expected Result: Verify the following information:(1) DMI time is updated only its offset time
             Test Step Comment: (1) MMI_gen 12062; MMI_gen 2421 (partly: VAP acting as NTP server);MMI_gen 11284;Note: ‘EVC-TBD’ will be provided by VSIS in order to support VAP interface 
             */
-            // Call generic Check Results Method
-            DmiExpectedResults.Verify_the_following_information1_DMI_time_is_updated_only_its_offset_time(this);
+            XML_29_3(msgType.msgType1);
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI just changes the local time displayed, adjusted by the offset time (+ 1 1/4 h)");
 
             /*
             Test Step 4
@@ -96,9 +106,11 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:The set clock window is opened(1) The DMI date (yyyy-mm-dd) shall not be changed by test script
             Test Step Comment: (1) MMI_gen 12062; MMI_gen 11284;
             */
-            // Call generic Action Method
-            DmiActions.Select_Settings_button_and_press_Set_clock_button(this);
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is unchanged.");
 
             /*
             Test Step 5
@@ -106,9 +118,12 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:(1) DMI time is updated only its offset time
             Test Step Comment: (1) MMI_gen 12062; MMI_gen 2421 (partly: VAP acting as NTP server);MMI_gen 11284;Note: ‘EVC-TBD’ will be provided by VSIS in order to support VAP interface
             */
-            // Call generic Check Results Method
-            DmiExpectedResults.Verify_the_following_information1_DMI_time_is_updated_only_its_offset_time(this);
+            DmiActions.ShowInstruction(this, @"Close the ‘Set clock’ window");
 
+            XML_29_3(msgType.msgType2);
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI just changes the local time displayed, adjusted by the offset time (- 1 1/4 h)");
 
             /*
             Test Step 6
@@ -116,7 +131,11 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information:The set clock window is opened(1) The DMI date (yyyy-mm-dd) is not be changed by test script
             Test Step Comment: (1) MMI_gen 12062; MMI_gen 11284;
             */
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
 
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is unchanged.");
 
             /*
             Test Step 7
@@ -124,7 +143,62 @@ namespace Testcase.DMITestCases
             Expected Result: Same as the test result in step 1 to 6
             Test Step Comment: MMI_gen 58;
             */
+            DmiActions.ShowInstruction(this, @"Power down the system, wait at least 10s then power up the system.");
 
+            // force SoM as above with Cabin 2
+            DmiActions.Start_ATP();
+
+            // Set train running number, cab 1 active, and other defaults
+            DmiActions.Activate_Cabin_1(this);
+
+            // Set driver ID
+            DmiActions. Set_Driver_ID(this, "1234");
+
+            // Set to level 1 and SR mode
+            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Level = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_LEVEL.L1;
+            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.StaffResponsible;
+
+            // Enable standard buttons including Start, and display Default window.
+            DmiActions.Finished_SoM_Default_Window(this);
+
+            // Ignore Step 1 as above...
+
+            // Repeat Step 2
+            EVC30_MMIRequestEnable.SendBlank();
+            EVC30_MMIRequestEnable.MMI_NID_WINDOW = 0;      // Default window
+            EVC30_MMIRequestEnable.MMI_Q_REQUEST_ENABLE_HIGH = EVC30_MMIRequestEnable.EnabledRequests.SetLocalTimeDateAndOffset;
+            EVC30_MMIRequestEnable.Send();
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays in SR mode, Level 1.");
+
+            // Repeat Step 3
+            XML_29_3(msgType.msgType1);
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI just changes the local time displayed, adjusted by the offset time (+ 1 1/4 h)");
+
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
+
+            // Repeat Step 4
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is unchanged.");
+
+            // Repeat Step 5
+            DmiActions.ShowInstruction(this, @"Close the ‘Set clock’ window");
+
+            XML_29_3(msgType.msgType2);
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI just changes the local time displayed, adjusted by the offset time (- 1 1/4 h)");
+
+            // Repeat Step 6
+            DmiActions.ShowInstruction(this, @"Press the ‘Settings’ button, then press the ‘Set clock’ button");
+
+            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+                                "1. DMI displays the Set clock window." + Environment.NewLine +
+                                "2. The date displayed is unchanged.");
 
             /*
             Test Step 8
@@ -132,8 +206,34 @@ namespace Testcase.DMITestCases
             Expected Result: 
             */
 
-
             return GlobalTestResult;
         }
+
+        #region XML_29_3_DMI_Test_Specification
+
+        private enum msgType : byte
+        {
+            msgType1,
+            msgType2
+        }
+
+        private void XML_29_3(msgType packetSelector)
+        {
+            switch (packetSelector)
+            {
+                case msgType.msgType1:
+                    EVC3_MMISetTimeATP.MMI_T_UTC = 946728000;
+                    EVC3_MMISetTimeATP.MMI_T_ZONE_OFFSET = 5;
+                    break;
+                case msgType.msgType2:
+                    EVC3_MMISetTimeATP.MMI_T_UTC = 946771200;
+                    EVC3_MMISetTimeATP.MMI_T_ZONE_OFFSET = -5; 
+                    break;
+            }
+
+            EVC3_MMISetTimeATP.Send();
+        }
+
+        #endregion
     }
 }
