06c88f686b2510ac29261d3896cf086f099fb1d0
sbeesley <simon.beesley@rail.bombardier.com>
2017-11-02 13:49:42 +0000

    test 22.4.8 (7) changed values to display TC13.

--- Testcase/DMITestCases/22 Planning Area in Main Area D/22.4/22.4.8 PA_Track_Condition_Switch_off_magnetic_shoe_brake_in_Sub_Area_D2_and_B3.cs
+++ Testcase/DMITestCases/22 Planning Area in Main Area D/22.4/22.4.8 PA_Track_Condition_Switch_off_magnetic_shoe_brake_in_Sub_Area_D2_and_B3.cs
@@ -163,8 +163,7 @@ namespace Testcase.DMITestCases
             EVC32_MMITrackConditions.Send();
 
             EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 0;
-            trackCondition.MMI_Q_TRACKCOND_STEP = Variables.MMI_Q_TRACKCOND_STEP.AnnounceArea;
-            trackCondition.MMI_Q_TRACKCOND_ACTION_START = Variables.MMI_Q_TRACKCOND_ACTION.WithoutDriverAction;
+            trackCondition.MMI_Q_TRACKCOND_STEP = Variables.MMI_Q_TRACKCOND_STEP.InsideArea_Active;
             EVC32_MMITrackConditions.Send();
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
