325d5864fa62139e66afacb2ed888c2c7fdd9d21
sbeesley <simon.beesley@rail.bombardier.com>
2017-11-16 13:22:43 +0000

    test 17.13 working but no slip/slide enabled in DMI.

--- Testcase/DMITestCases/17 Train Speed/17.13/17.13 Slide_Indication.cs
+++ Testcase/DMITestCases/17 Train Speed/17.13/17.13 Slide_Indication.cs
@@ -143,6 +143,9 @@ namespace Testcase.DMITestCases
 
         private void XML_12_13(msgType type)
         {
+            //SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0x0;
+            //SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0x0;
+
             if (type == msgType.typea)
             {
                 EVC1_MMIDynamic.MMI_M_SLIDE = 1;
@@ -157,8 +160,6 @@ namespace Testcase.DMITestCases
                 EVC1_MMIDynamic.MMI_O_IML = 0;
                 EVC1_MMIDynamic.MMI_V_INTERVENTION = 0;
 
-                SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0x0;
-                SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0x0;
             }
             else if (type == msgType.typeb)
             {
@@ -173,9 +174,6 @@ namespace Testcase.DMITestCases
                 EVC1_MMIDynamic.MMI_O_BRAKETARGET = 0;
                 EVC1_MMIDynamic.MMI_O_IML = 0;
                 EVC1_MMIDynamic.MMI_V_INTERVENTION = 0;
-
-                SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0x0;
-                SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0x0;
             }
             else if (type == msgType.typec)
             {
@@ -190,9 +188,6 @@ namespace Testcase.DMITestCases
                 EVC1_MMIDynamic.MMI_O_BRAKETARGET = 0;
                 EVC1_MMIDynamic.MMI_O_IML = 0;
                 EVC1_MMIDynamic.MMI_V_INTERVENTION = 0;
-
-                SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0x0;
-                SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0x0;
             }
         }
         #endregion
