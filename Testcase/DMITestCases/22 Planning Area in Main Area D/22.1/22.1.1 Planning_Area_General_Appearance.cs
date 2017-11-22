094178badd338eb2557efb2e9ad9d421f9fe98f5
sbeesley <simon.beesley@rail.bombardier.com>
2017-11-22 14:33:18 +0000

    test 22.1.1 class renamed.

--- Testcase/DMITestCases/22 Planning Area in Main Area D/22.1/22.1.1 Planning_Area_General_Appearance.cs
+++ Testcase/DMITestCases/22 Planning Area in Main Area D/22.1/22.1.1 Planning_Area_General_Appearance.cs
@@ -41,7 +41,7 @@ namespace Testcase.DMITestCases
     /// Used files:
     /// 17_1_1.tdg
     /// </summary>
-    public class TC_17_1_1_Planning_Area : TestcaseBase
+    public class TC_ID_17_1_1_Planning_Area : TestcaseBase
     {
         public override void PreExecution()
         {
