26de395b9d1209bac7dd3a19a61b5755efcb4eb9
Sam Sivahary <ext.samson.sivahary@rail.bombardier.com>
2018-01-17 14:11:34 +0100

    TC_5_12_2 updated

--- Testcase/DMITestCases/10 General Appearance/10.12/10.12.2 Close_Next_Previous_and_Yes_Buttons.cs
+++ Testcase/DMITestCases/10 General Appearance/10.12/10.12.2 Close_Next_Previous_and_Yes_Buttons.cs
@@ -30,8 +30,8 @@ namespace Testcase.DMITestCases
             base.PreExecution();
 
             // Test system is powered on.
-            EVC0_MMIStartATP.Evc0Type = EVC0_MMIStartATP.EVC0Type.GoToIdle;
-            EVC0_MMIStartATP.Send();
+            DmiActions.Start_ATP();
+
         }
 
         public override void PostExecution()
@@ -50,14 +50,18 @@ namespace Testcase.DMITestCases
             /*
             Test Step 1
             Action: Activate cabin A
-            Expected Result: DMI displays Driver ID window.Verify the following information,Delete button NA21 is shown as enabled state in area G.The disabled Close button NA12 is display in area G
+            Expected Result: DMI displays Driver ID window.
+            Verify the following information,
+            Delete button NA21 is shown as enabled state in area G.
+            The disabled Close button NA12 is display in area G
             Test Step Comment: (1) MMI_gen 4392 (partly: bullet e, delete NA21); MMI_gen 4440 (partly: delete, enabled);(2) MMI_gen 4440 (partly: other navigation buttons); MMI_gen 4396 (partly: close, NA12); MMI_gen 4392 (partly: bullet a, close button);  MMI_gen 4395 (partly: close button, disabled);
             */
             // Call generic Action Method
             DmiActions.Activate_Cabin_1(this);
-            EVC14_MMICurrentDriverID.MMI_Q_ADD_ENABLE = EVC14_MMICurrentDriverID.MMI_Q_ADD_ENABLE_BUTTONS.Settings;
-            EVC14_MMICurrentDriverID.MMI_Q_CLOSE_ENABLE = Variables.MMI_Q_CLOSE_ENABLE.Enabled;
-            EVC14_MMICurrentDriverID.Send();
+
+            DmiActions.Set_Driver_ID(this, "1234");
+            DmiActions.Send_SB_Mode(this);
+            DmiExpectedResults.Driver_ID_window_displayed_in_SB_mode(this);
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI displays the Driver ID window." + Environment.NewLine +
@@ -69,14 +73,22 @@ namespace Testcase.DMITestCases
             Action: Enter the Driver ID ‘12345678’
             Expected Result: The value of an input field is displayed correspond with entered data
             */
-            DmiActions.ShowInstruction(this, @"Enter the Driver ID ‘12345678’");
+            DmiActions.ShowInstruction(this, "Enter the Driver ID ‘12345678’.");
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 @"1. The Input Field is displayed with the value ‘1234 5678’");
 
             /*
             Test Step 3
-            Action: Press and hold ‘Delete’ button up to 2 second.Note:Stopwatch is required
-            Expected Result: Verify the following information,While press and hold button less than 1.5 secSound ‘Click’ is played once.The state of button is changed to ‘Pressed’ and immediately back to ‘Enabled’ state.The last character of Driver ID is  deleted from the input field.While press and hold button over 1.5 secThe state ‘pressed’ and ‘released’ are switched repeatly while button is pressed and the characters are removed from an input field repeatly refer to pressed state.The sound ‘Click’ is played repeatly while button is pressed
+            Action: Press and hold ‘Delete’ button up to 2 second.
+            Note:Stopwatch is required
+            Expected Result: Verify the following information,
+            While press and hold button less than 1.5 sec
+                Sound ‘Click’ is played once.
+                The state of button is changed to ‘Pressed’ and immediately back to ‘Enabled’ state.
+                The last character of Driver ID is  deleted from the input field.
+            While press and hold button over 1.5 sec
+                The state ‘pressed’ and ‘released’ are switched repeatly while button is pressed and the characters are removed from an input field repeatly refer to pressed state.
+                The sound ‘Click’ is played repeatly while button is pressed
             Test Step Comment: (1) MMI_gen 4393 (partly: delete, MMI_gen 4384 (partly: sound ‘Click’));(2) MMI_gen 4393 (partly: MMI_gen 4384 (partly: Change to state ‘Pressed’ and immediately back to state ‘Enabled’));(3) MMI_gen 4393 (partly: delete, MMI_gen 4384 (partly: ETCS-MMI’s function associated to the button));(4) MMI_gen 4393 (partly: delete, MMI_gen 4386 (partly: visual of repeat function));(5) MMI_gen 4393 (partly: delete, MMI_gen 4386 (partly: audible of repeat function));
             */
             DmiActions.ShowInstruction(this, @"Press and hold the ‘Delete’ button then release the button within 1.5s");
@@ -100,7 +112,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 4
             Action: Release the pressed button
-            Expected Result: Verify the following information,The state of pressed button is changed to ‘Enabled’ state
+            Expected Result: Verify the following information,
+            The state of pressed button is changed to ‘Enabled’ state
             Test Step Comment: (1) MMI_gen 4393 (partly: delete, MMI_gen 4384 (partly: ETCS-MMI’s function associated to the button));
             */
             DmiActions.ShowInstruction(this, "Release the pressed button");
@@ -110,66 +123,62 @@ namespace Testcase.DMITestCases
 
             /*
             Test Step 5
-            Action: Perform the following procedure,Enter and confirm Driver IDPerform brake testSelect and confirm Level 1
+            Action: Perform the following procedure,
+            Enter and confirm Driver ID
+            Perform brake test
+            Select and confirm Level 1
             Expected Result: DMI displays the Main window
             */
-            DmiActions.ShowInstruction(this, "Enter and accept Driver ID");
 
-            EVC20_MMISelectLevel.MMI_Q_CLOSE_ENABLE = Variables.MMI_Q_CLOSE_ENABLE.Disabled;
-            EVC20_MMISelectLevel.MMI_Q_LEVEL_NTC_ID = new Variables.MMI_Q_LEVEL_NTC_ID[]
-                {Variables.MMI_Q_LEVEL_NTC_ID.ETCS_Level};
-            EVC20_MMISelectLevel.MMI_M_CURRENT_LEVEL = new Variables.MMI_M_CURRENT_LEVEL[]
-                {Variables.MMI_M_CURRENT_LEVEL.NotLastUsedLevel};
-            EVC20_MMISelectLevel.MMI_M_LEVEL_FLAG = new Variables.MMI_M_LEVEL_FLAG[]
-                {Variables.MMI_M_LEVEL_FLAG.MarkedLevel};
-            EVC20_MMISelectLevel.MMI_M_INHIBITED_LEVEL = new Variables.MMI_M_INHIBITED_LEVEL[]
-                {Variables.MMI_M_INHIBITED_LEVEL.NotInhibited};
-            EVC20_MMISelectLevel.MMI_M_INHIBIT_ENABLE = new Variables.MMI_M_INHIBIT_ENABLE[]
-                {Variables.MMI_M_INHIBIT_ENABLE.AllowedForInhibiting};
-            EVC20_MMISelectLevel.MMI_M_LEVEL_NTC_ID = new Variables.MMI_M_LEVEL_NTC_ID[]
-                {Variables.MMI_M_LEVEL_NTC_ID.L1};
-            EVC20_MMISelectLevel.Send();
+            DmiExpectedResults.Driver_ID_entered(this);
 
-            DmiActions.ShowInstruction(this, " Select and accept Level 1");
+            DmiActions.Request_Brake_Test(this);
+            DmiActions.ShowInstruction(this, "Perform Brake Test");
+            DmiActions.Perform_Brake_Test(this, 2);
+            Wait_Realtime(5000);
+            DmiActions.Display_Brake_Test_Successful(this, 3);
 
-            EVC30_MMIRequestEnable.SendBlank();
-            EVC30_MMIRequestEnable.MMI_NID_WINDOW = EVC30_MMIRequestEnable.WindowID.Main;
-            EVC30_MMIRequestEnable.MMI_Q_REQUEST_ENABLE_HIGH =
-                EVC30_MMIRequestEnable.EnabledRequests.Start | Variables.standardFlags;
-            EVC30_MMIRequestEnable.Send();
+            DmiActions.Display_Level_Window(this);
+            DmiActions.Delete_Brake_Test_Successful(this, 3);
 
-            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
-                                "1. DMI displays the Main window.");
+            DmiActions.ShowInstruction(this, "Select and enter Level 1");
+
+            DmiActions.Display_Main_Window_with_Start_button_not_enabled(this);
+            DmiExpectedResults.Main_Window_displayed(this, false);
 
             /*
             Test Step 6
             Action: Press ‘Driver ID’ button
-            Expected Result: DMI displays the Driver ID window.The enabled Close button NA11 is display in area G
+            Expected Result: DMI displays the Driver ID window.
+            The enabled Close button NA11 is display in area G
             Test Step Comment: (1) MMI_gen 4392 (partly: bullet a, close button, NA11); MMI_gen 4396 (partly: close, NA11); 
             */
             DmiActions.ShowInstruction(this, "Press the ‘Driver ID’ button");
 
-            EVC14_MMICurrentDriverID.MMI_Q_ADD_ENABLE = EVC14_MMICurrentDriverID.MMI_Q_ADD_ENABLE_BUTTONS.Settings;
-            EVC14_MMICurrentDriverID.MMI_Q_CLOSE_ENABLE = Variables.MMI_Q_CLOSE_ENABLE.Enabled;
-            EVC14_MMICurrentDriverID.Send();
+            DmiActions.Set_Driver_ID(this, Telegrams.DMItoEVC.EVC104_MMINewDriverData.Get_X_DRIVER_ID);
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI displays the Driver ID  window." + Environment.NewLine +
                                 @"2. The ‘Close’ button NA11 is displayed enabled in area G.");
             /*
             Test Step 7
-            Action: Perform the following procedure,Press ‘Close’ button.Press ‘Train data’ button
-            Expected Result: DMI displays the first page of  the  train data entry.The enabled Close button NA 11 is display in area G.The enabled Next button NA17 is display in area G.The disabled Previous button NA19 is display in area G
+            Action: Perform the following procedure,
+            Press ‘Close’ button.
+            Press ‘Train data’ button
+            Expected Result: DMI displays the first page of  the  train data entry.
+            The enabled Close button NA 11 is display in area G.
+            The enabled Next button NA17 is display in area G.
+            The disabled Previous button NA19 is display in area G
             Test Step Comment: (1) MMI_gen 4392 (partly: bullet a, close button, NA11);(2) MMI_gen 4392 (partly: bullet c, next button, NA17);(3) MMI_gen 4394 (partly: previous, disabled); MMI_gen 4396 (partly: previous, NA19); MMI_gen 4392 (partly: bullet d, previous button);
             */
-            EVC30_MMIRequestEnable.SendBlank();
-            EVC30_MMIRequestEnable.MMI_NID_WINDOW = EVC30_MMIRequestEnable.WindowID.Main;
-            EVC30_MMIRequestEnable.MMI_Q_REQUEST_ENABLE_HIGH = EVC30_MMIRequestEnable.EnabledRequests.TrainData;
-            EVC30_MMIRequestEnable.Send();
 
-            DmiActions.ShowInstruction(this, @"Press the ‘Close’ button. Press the ‘Train data’ button");
+            DmiActions.ShowInstruction(this, @"Press the ‘Close’ button.");
 
-            DmiActions.Send_EVC6_MMICurrentTrainData_FixedDataEntry(this, new[] {"FLU", "RLU", "Rescue"}, 2);
+            DmiActions.Display_Main_Window_with_Start_button_not_enabled(this);
+            DmiActions.ShowInstruction(this, @"Press ‘Train data’ button");
+
+            DmiActions.Display_Fixed_Train_Data_Window(this);
+            DmiExpectedResults.Train_data_window_displayed(this);
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI displays the first page of Train data entry." + Environment.NewLine +
@@ -179,7 +188,9 @@ namespace Testcase.DMITestCases
             /*
             Test Step 8
             Action: Press and hold Next button
-            Expected Result: Verify the following information,The Next button is shown as pressed state.The sound ‘click’ is played once
+            Expected Result: Verify the following information,
+            The Next button is shown as pressed state.
+            The sound ‘click’ is played once
             Test Step Comment: (1) MMI_gen 9391 (partly: Next, MMI_gen 4381 (partly: change to state ‘Pressed’ as long as remain actuated));
             */
             DmiActions.ShowInstruction(this, @"Press and hold the ‘Next’ button");
@@ -191,7 +202,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 9
             Action: Slide out Next button
-            Expected Result: Verify the following information,The Next button becomes the ‘Enabled’ state without a sound
+            Expected Result: Verify the following information,
+            The Next button becomes the ‘Enabled’ state without a sound
             Test Step Comment: (1) MMI_gen 9391 (partly: Next, MMI_gen 4382 (partly: state ‘Enabled’ when slide out with force applied, no sound));
             */
             DmiActions.ShowInstruction(this, @"Whilst keeping the ‘Next’ button pressed, drag outside its area");
@@ -203,7 +215,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 10
             Action: Slide back into Next button
-            Expected Result: Verify the following information,The Next button is shown as pressed state and no sound ‘Click’ is played
+            Expected Result: Verify the following information,
+            The Next button is shown as pressed state and no sound ‘Click’ is played
             Test Step Comment: (1) MMI_gen 9391 (partly: Next, MMI_gen 4382 (partly: state ‘Pressed’ when slide back, no sound));
             */
             DmiActions.ShowInstruction(this, @"Whilst keeping the ‘Next’ button pressed, drag it back inside its area");
@@ -215,7 +228,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 11
             Action: Release Next button
-            Expected Result: Verify the following information,DMI display the next page of Train data window
+            Expected Result: Verify the following information,
+            DMI display the next page of Train data window
             Test Step Comment: (1) MMI_gen 9391 (partly: Next, MMI_gen 4381 (partly: exit state ‘Pressed’, execute function associated to the button));
             */
             DmiActions.ShowInstruction(this, @"Release the ‘Next’ button");
@@ -226,7 +240,9 @@ namespace Testcase.DMITestCases
             /*
             Test Step 12
             Action: Press the next button until the last page of Train data window
-            Expected Result: DMI displays the last page of Train data window.Verify the following information,The disabled Next button NA18.2 is display in area G. The enalbed Previous button NA18 is display in area G
+            Expected Result: DMI displays the last page of Train data window.
+            Verify the following information,The disabled Next button NA18.2 is display in area G. 
+            The enalbed Previous button NA18 is display in area G
             Test Step Comment: (1) MMI_gen 4394 (partly: next, disabled); MMI_gen 4396 (partly: next, NA18.2);(2) MMI_gen 4392 (partly: bullet d, previous button, NA18); MMI_gen 4396 (partly: previous, NA18);
             */
             DmiActions.ShowInstruction(this,
@@ -240,7 +256,9 @@ namespace Testcase.DMITestCases
             /*
             Test Step 13
             Action: Press and hold Previous button
-            Expected Result: Verify the following information,The Previous button is shown as pressed state.The sound ‘click’ is played once
+            Expected Result: Verify the following information,
+            The Previous button is shown as pressed state.
+            The sound ‘click’ is played once
             Test Step Comment: (1) MMI_gen 9391 (partly: Next, MMI_gen 4381 (partly: change to state ‘Pressed’ as long as remain actuated));
             */
             DmiActions.ShowInstruction(this, @"Press and hold the ‘Previous’ button");
@@ -252,7 +270,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 14
             Action: Slide out Previous button
-            Expected Result: Verify the following information,The Previous button becomes the ‘Enabled’ state without a sound
+            Expected Result: Verify the following information,
+            The Previous button becomes the ‘Enabled’ state without a sound
             Test Step Comment: (1) MMI_gen 9391 (partly: Previous, MMI_gen 4382 (partly: state ‘Enabled’ when slide out with force applied, no sound));
             */
             DmiActions.ShowInstruction(this, @"Whilst keeping the ‘Previous’ button pressed, drag outside its area");
@@ -264,7 +283,8 @@ namespace Testcase.DMITestCases
             /*
             Test Step 15
             Action: Slide back into Previous button
-            Expected Result: Verify the following information,The Previous button is shown as pressed state and no sound ‘Click’ is played
+            Expected Result: Verify the following information,
+            The Previous button is shown as pressed state and no sound ‘Click’ is played
             Test Step Comment: (1) MMI_gen 9391 (partly: Previous, MMI_gen 4382 (partly: state ‘Pressed’ when slide back, no sound));
             */
             DmiActions.ShowInstruction(this,
@@ -277,7 +297,9 @@ namespace Testcase.DMITestCases
             /*
             Test Step 16
             Action: Release Previous button
-            Expected Result: Verify the following information,DMI display the previous page of Train data window.The enabled Next button NA17 is display in area G
+            Expected Result: Verify the following information,
+            DMI display the previous page of Train data window.
+            The enabled Next button NA17 is display in area G
             Test Step Comment: (1) MMI_gen 9391 (partly: Previous, MMI_gen 4381 (partly: exit state ‘Pressed’, execute function associated to the button));(2) MMI_gen 4396 (partly: next, NA17);
             */
             DmiActions.ShowInstruction(this, @"Release the ‘Previous’ button");
@@ -293,6 +315,8 @@ namespace Testcase.DMITestCases
             */
             // Call generic Action Method
             DmiActions.ShowInstruction(this, @"Press the ‘Close’ button");
+            DmiActions.Display_Main_Window_with_Start_button_not_enabled(this);
+            DmiExpectedResults.Main_Window_displayed(this, true);
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI displays the Main window.");
