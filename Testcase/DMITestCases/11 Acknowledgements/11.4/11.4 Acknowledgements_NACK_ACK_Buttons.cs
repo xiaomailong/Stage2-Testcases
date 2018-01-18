348b229b4a68f613cb7c71c27f320339adc063b8
Sam Sivahary <ext.samson.sivahary@rail.bombardier.com>
2018-01-18 16:15:59 +0100

    TC_6_4 updated

--- Testcase/DMITestCases/11 Acknowledgements/11.4/11.4 Acknowledgements_NACK_ACK_Buttons.cs
+++ Testcase/DMITestCases/11 Acknowledgements/11.4/11.4 Acknowledgements_NACK_ACK_Buttons.cs
@@ -1,5 +1,6 @@
 using System;
 using Testcase.Telegrams.EVCtoDMI;
+using Testcase.Telegrams.DMItoEVC;
 
 
 namespace Testcase.DMITestCases
@@ -33,7 +34,10 @@ namespace Testcase.DMITestCases
 
             // Change the background-colour of the style below in gen_buttonStyle_type_4.xml to be #BF0002, STYLE class="normal", STYLE class="flash"
 
-            // System is powered onCabin is activatedPerform SoM until level 1 is selected and confirmedMain window is closed.
+            // System is powered on
+            // Cabin is activated
+            // Perform SoM until level 1 is selected and confirmed
+            // Main window is closed.
             DmiActions.Complete_SoM_L1_SB(this);
         }
 
@@ -52,8 +56,16 @@ namespace Testcase.DMITestCases
 
             /*
             Test Step 1
-            Action: Use the test script file 6_4_a.xml to send EVC-8 with MMI_Q_TEXT = 527MMI_Q_TEXT_CRITERIA = 2MMI_I_TEXT = 1MMI_Q_TEXT_CLASS = 1
-            Expected Result: DMI displays the text message ‘Brake test aborted, perform new Test?’ with an acknowledgement option in sub-area E5-E9.Verify the following information,(1)  The ‘ACK’ button is labelled with text ‘ACK in yellow colour.(2)  The 'NACK' button is labelled with text 'NACK' in yellow colour.(3)   The background colour of both button 'ACK' and 'NACK' are red.
+            Action: Use the test script file 6_4_a.xml to send EVC-8 with 
+            MMI_Q_TEXT = 527
+            MMI_Q_TEXT_CRITERIA = 2
+            MMI_I_TEXT = 1
+            MMI_Q_TEXT_CLASS = 1
+            Expected Result: DMI displays the text message ‘Brake test aborted, perform new Test?’ with an acknowledgement option in sub-area E5-E9.
+            Verify the following information,
+            (1)  The ‘ACK’ button is labelled with text ‘ACK in yellow colour.
+            (2)  The 'NACK' button is labelled with text 'NACK' in yellow colour.
+            (3)  The background colour of both button 'ACK' and 'NACK' are red.
             
             Test Step Comment: (1) MMI_gen 4468 (partly: ‘ACK’ in yellow);(2) MMI_gen 4507 (partly: 'NACK' in yellow colour);(3) MMI_gen 4468 (partly: configured background colour); MMI_gen 4507 (partly: configured background colour);
 
@@ -70,11 +82,17 @@ namespace Testcase.DMITestCases
             /*
             Test Step 2
             Action: Press and hold 'NACK' button.
-            Expected Result: Verify the following information,(1)   The sound ‘Click’ played once.(2)   The 'NACK' button is shown as pressed state, the border of button is removed.(3)   Use the log file to confirm that DMI send EVC-111 with variable MMI_Q_BUTTON = 1 (Pressed) and MMI_T_BUTTONEVENT is not blank.
-
+            Expected Result: Verify the following information,
+            (1)   The sound ‘Click’ played once.
+            (2)   The 'NACK' button is shown as pressed state, the border of button is removed.
+            (3)   Use the log file to confirm that DMI send EVC-111 with variable MMI_Q_BUTTON = 1 (Pressed) and MMI_T_BUTTONEVENT is not blank.
             Test Step Comment: (1) MMI_gen 4470 (partly: 'NACK', MMI_gen 4381 (partly: the sound for Up-Type button)); MMI_gen 4256 (partly: Click sound);(2) MMI_gen 4470 (partly: 'NACK', MMI_gen 4381 (partly: change to state ‘Pressed’ as long as remain actuated));(3) MMI_gen 4470 (partly: MMI_gen 11387 (partly: send events of Pressed independently to ETCS), MMI_gen 11907 (partly: EVC-111, timestamp)); MMI_gen 3375; MMI_gen 3200 (partly: NACK button, pressed);
             */
             DmiActions.ShowInstruction(this, "Press and hold the ‘NACK’ button");
+
+            EVC111_MMIDriverMessageAck.MMI_I_TEXT = 1;
+            EVC111_MMIDriverMessageAck.MMI_Q_ACK = MMI_Q_ACK.NotAcknowledgeNO;
+            EVC111_MMIDriverMessageAck.MMI_Q_BUTTON = Variables.MMI_Q_BUTTON.Pressed;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. The ‘Click’ sound is played once." + Environment.NewLine +
@@ -87,7 +105,7 @@ namespace Testcase.DMITestCases
 
             Test Step Comment: MMI_gen 4470 (partly: 'NACK', MMI_gen 4382 (partly: state ‘Enabled’ when slide out with force applied, no sound))); MMI_gen 4374;
             */
-            DmiActions.ShowInstruction(this, "Whilst keeping the ‘NACK’ button pressed, drag outside the area");
+            DmiActions.ShowInstruction(this, "Whilst keeping the ‘NACK’ button pressed, drag outside the area");            
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. The button is displayed enabled." + Environment.NewLine +
@@ -109,18 +127,26 @@ namespace Testcase.DMITestCases
             /*
             Test Step 5
             Action: Release the 'NACK' button.	
-            Expected Result: Verify the following information,(1)    The Ack-buttons with yellow flashing frame and text message is disappear.(2)   Use the log file to confirm that DMI send EVC-111 with variable MMI_Q_BUTTON = 0 (Released) and MMI_T_BUTTONEVENT is not blank.	(1) MMI_gen 4470 (partly: 'NACK', MMI_gen 4381 (partly: exit state ‘Pressed’, execute function associated to the button)));
+            Expected Result: Verify the following information,
+            (1)   The Ack-buttons with yellow flashing frame and text message is disappear.
+            (2)   Use the log file to confirm that DMI send EVC-111 with variable MMI_Q_BUTTON = 0 (Released) and MMI_T_BUTTONEVENT is not blank.	
+            (1) MMI_gen 4470 (partly: 'NACK', MMI_gen 4381 (partly: exit state ‘Pressed’, execute function associated to the button)));
             
             Test Step Comment: (2) MMI_gen 4470 (partly: MMI_gen 11387 (partly: send events of Pressed independently to ETCS), MMI_gen 11907 (partly: EVC-111, timestamp)); MMI_gen 3375; MMI_gen 3200 (partly: NACK button, released);
             */
             DmiActions.ShowInstruction(this, "Release the ‘NACK’ button");
+
+            EVC111_MMIDriverMessageAck.MMI_I_TEXT = 1;
+            EVC111_MMIDriverMessageAck.MMI_Q_ACK = MMI_Q_ACK.NotAcknowledgeNO;
+            EVC111_MMIDriverMessageAck.MMI_Q_BUTTON = Variables.MMI_Q_BUTTON.Released;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI stops displaying the acknowledgement buttons.");
 
             /*
             Test Step 6
-            Action: Use the test script file 6_4_a.xml to send EVC-8.Then, perform action step 2-5 for 'ACK' button.            
+            Action: Use the test script file 6_4_a.xml to send EVC-8.
+            Then, perform action step 2-5 for 'ACK' button.            
             Expected Result: See the expected results of Step 2 – Step 5.	
 
             Test Step Comment: MMI_gen 4470 (partly: 'ACK'); MMI_gen 3375; MMI_gen 3200 (partly: ACK button);
@@ -129,6 +155,10 @@ namespace Testcase.DMITestCases
 
             // Repeat Step 2
             DmiActions.ShowInstruction(this, "Press and hold the ‘ACK’ button");
+
+            EVC111_MMIDriverMessageAck.MMI_I_TEXT = 1;
+            EVC111_MMIDriverMessageAck.MMI_Q_ACK = MMI_Q_ACK.AcknowledgeYES;
+            EVC111_MMIDriverMessageAck.MMI_Q_BUTTON = Variables.MMI_Q_BUTTON.Pressed;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. The ‘Click’ sound is played once." + Environment.NewLine +
@@ -151,13 +181,22 @@ namespace Testcase.DMITestCases
             // Repeat Step 5
             DmiActions.ShowInstruction(this, "Release the ‘NACK’ button");
 
+            EVC111_MMIDriverMessageAck.MMI_I_TEXT = 1;
+            EVC111_MMIDriverMessageAck.MMI_Q_ACK = MMI_Q_ACK.AcknowledgeYES;
+            EVC111_MMIDriverMessageAck.MMI_Q_BUTTON = Variables.MMI_Q_BUTTON.Released;
+
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI stops displaying the acknowledgement buttons.");
 
             /*
             Test Step 7
-            Action: Use the test script file 6_4_b.xml to send EVC-8 with,MMI_I_TEXT = 1MMI_Q_TEXT = 527MMI_Q_TEXT_CRITERIA = 0	MMI_Q_TEXT_CLASS = 1	
-            Expected Result: Verify the following information,(1)   DMI displays the text message ‘Brake test aborted, perform new Test?’ with a yellow flashing frame around sub-area E5-E9.	
+            Action: Use the test script file 6_4_b.xml to send EVC-8 with,
+            MMI_I_TEXT = 1
+            MMI_Q_TEXT = 527
+            MMI_Q_TEXT_CRITERIA = 0	
+            MMI_Q_TEXT_CLASS = 1	
+            Expected Result: Verify the following information,
+            (1)   DMI displays the text message ‘Brake test aborted, perform new Test?’ with a yellow flashing frame around sub-area E5-E9.	
 
             Test Step Comment: (1) MMI_gen 4468 (partly: Note); MMI_gen 3374 (partly: Text acknowledgement, visible, not faulty);
             */
@@ -169,7 +208,11 @@ namespace Testcase.DMITestCases
             /*
             Test Step 8
             Action: Press and hold sub-area E5.	
-            Expected Result: Verify the following information,(1)   The sound ‘Click’ played once.(2)   The sub-area E5 is shown as pressed state, the border of button is removed.(3)   Use the log file to confirm that DMI send EVC-101 with variable MMI_Q_BUTTON = 1 (Pressed) and MMI_T_BUTTONEVENT is not blank.	(1) MMI_gen 3200 (partly: Text acknowledgement, MMI_gen 4381 (partly: the sound for Up-Type button));
+            Expected Result: Verify the following information,
+            (1)   The sound ‘Click’ played once.
+            (2)   The sub-area E5 is shown as pressed state, the border of button is removed.
+            (3)   Use the log file to confirm that DMI send EVC-101 with variable MMI_Q_BUTTON = 1 (Pressed) and MMI_T_BUTTONEVENT is not blank.	
+            (1) MMI_gen 3200 (partly: Text acknowledgement, MMI_gen 4381 (partly: the sound for Up-Type button));
             
             Test Step Comment: (2) MMI_gen 3200 (partly: Text acknowledgement, MMI_gen 4381 (partly: change to state ‘Pressed’ as long as remain actuated));(3) MMI_gen 3200 (partly: Text acknowledgement, pressed, MMI_gen 11387 (partly: send events of Pressed independently to ETCS), MMI_gen 11907 (partly: EVC-101, timestamp)); (Continue from step 7)Send EVC-8 with, MMI_Q_TEXT = 269MMI_Q_TEXT_CRITERIA = 1MMI_I_TEXT = 8The display information on DMI still not change, ST01 symbol is displayed on sub-area C9
             */
@@ -208,7 +251,10 @@ namespace Testcase.DMITestCases
             /*
             Test Step 11
             Action: Release the pressed area.	
-            Expected Result: Verify the following information,(1)   The yellow flashing frame is removed, DMI still display text message 'Brake test aborted, perform new Test?' in sub-area E5.(2)   Use the log file to confirm that DMI send EVC-101 with variable MMI_Q_BUTTON = 0 (Released) and MMI_T_BUTTONEVENT is not blank.	(1) MMI_gen 4499 (partly: text message step back as non-acknowledgementable);
+            Expected Result: Verify the following information,
+            (1)   The yellow flashing frame is removed, DMI still display text message 'Brake test aborted, perform new Test?' in sub-area E5.
+            (2)   Use the log file to confirm that DMI send EVC-101 with variable MMI_Q_BUTTON = 0 (Released) and MMI_T_BUTTONEVENT is not blank.	
+            (1) MMI_gen 4499 (partly: text message step back as non-acknowledgementable);
             
             Test Step Comment: (2) MMI_gen 3200 (partly: Text acknowledgement, released, MMI_gen 11387 (partly: send events of Released independently to ETCS), MMI_gen 11907 (partly: EVC-101, timestamp));        
             */
