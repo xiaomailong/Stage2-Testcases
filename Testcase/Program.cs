27956596638a591e2953213471d6ddfeffe82434
Johan Sonesson <johan@traintech.eu>
2018-02-02 19:01:20 +0000

    Add method to run through all testcases

--- Testcase/Program.cs
+++ Testcase/Program.cs
@@ -1,4 +1,8 @@
-﻿using BT_CSB_Tools;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BT_CSB_Tools;
 using Testcase.DMITestCases;
 
 namespace Testcase
@@ -7,10 +11,37 @@ namespace Testcase
     {
         static void Main(string[] args)
         {
+            if (true)
+            {
+                // Create a list of TestCases to ignore
+                var ignorelist = new List<string>
+                {
+                    "TestcaseBase"
+                };
 
-            TestcaseRunner.AddTestcase(typeof(TC_ID_6_1_Acknowledgements));
-            //TestcaseRunner.AddTestcase(typeof(TestcaseBase));
-            TestcaseRunner.RunTestcases(args);
+                // Get all classes under Testcase.Scenarios
+                var typelist = GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Testcase.DMITestCases");
+
+                // Filter typelist through ignorelist and add them to Testcaserunner
+                foreach (var type in typelist.Where(t => !ignorelist.Contains(t.Name)))
+                    TestcaseRunner.AddTestcase(type);
+                
+                TestcaseRunner.RunTestcases(args);
+            }
+            else
+            {
+                TestcaseRunner.AddTestcase(typeof(TC_ID_6_1_Acknowledgements));
+                //TestcaseRunner.AddTestcase(typeof(TestcaseBase));
+                TestcaseRunner.RunTestcases(args);
+            }
+        }
+
+        private static Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+        {
+            return
+                assembly.GetTypes()
+                    .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                    .ToArray();
         }
     }
 }
