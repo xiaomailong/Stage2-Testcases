745fc4de9f3d7754b9626ea0d6463c619f4d0d4a
Dave-Dangerous <ext.dave.smith1@rail.bombardier.com>
2018-02-08 15:03:06 +0000

    Seventh batch of uniqueid changes

--- Testcase/DMITestCases/18 Brake/18.1/18.1.7.1 Distance_to_Target_Appearance_of_Distance_to_Target_in_FS_mode.cs
+++ Testcase/DMITestCases/18 Brake/18.1/18.1.7.1 Distance_to_Target_Appearance_of_Distance_to_Target_in_FS_mode.cs
@@ -29,7 +29,7 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // This identifier shall match the identity of the first testcasestep of the testcase in Doors
-            UniqueIdentifier = 0;
+            UniqueIdentifier = 21951;
             // Testcase entrypoint
 
             StartUp();
--- Testcase/DMITestCases/20 Status Information to The Driver/20.2/20.2.2 ETCS_Level_in_Sub_Area_C8_Level_2_and_Level_3_by_driver_selection.cs
+++ Testcase/DMITestCases/20 Status Information to The Driver/20.2/20.2.2 ETCS_Level_in_Sub_Area_C8_Level_2_and_Level_3_by_driver_selection.cs
@@ -26,7 +26,7 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // This identifier shall match the identity of the first testcasestep of the testcase in Doors
-            UniqueIdentifier = 0;
+            UniqueIdentifier = 22380;
             // Testcase entrypoint
             StartUp();
 
