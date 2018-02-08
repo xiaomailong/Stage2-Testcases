690e08f99af1d1689a0fd4b19497c61275701e7b
Dave-Dangerous <ext.dave.smith1@rail.bombardier.com>
2018-02-08 15:24:09 +0000

    Final batch of Uniqueid changes

--- Testcase/DMITestCases/17 Train Speed/17.3/17.3.8 Speed_Pointer_Colour_of_speed_pointer_in_OS_mode.cs
+++ Testcase/DMITestCases/17 Train Speed/17.3/17.3.8 Speed_Pointer_Colour_of_speed_pointer_in_OS_mode.cs
@@ -38,7 +38,7 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // This identifier shall match the identity of the first testcasestep of the testcase in Doors
-            UniqueIdentifier = 0;
+            UniqueIdentifier = 21546;
             // Testcase entrypoint
 
             StartUp();
--- Testcase/DMITestCases/17 Train Speed/17.7/17.7.2 Release_Speed_Digital_is_removed_when_communication_between_ETCS_Onboard_and_DMI_is_lost.cs
+++ Testcase/DMITestCases/17 Train Speed/17.7/17.7.2 Release_Speed_Digital_is_removed_when_communication_between_ETCS_Onboard_and_DMI_is_lost.cs
@@ -30,7 +30,7 @@ namespace Testcase.DMITestCases
         public override bool TestcaseEntryPoint()
         {
             // This identifier shall match the identity of the first testcasestep of the testcase in Doors
-            UniqueIdentifier = 0;
+            UniqueIdentifier = 21738;
             // Testcase entrypoint
 
             MakeTestStepHeader(1, UniqueIdentifier++, "Activate cabin A", "ATP is in SB mode.DMI displays in SB mode");
