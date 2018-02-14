8539e8d8c26ce9d333b23ac7b7d44f3a092d13c3
Robert van Vaardegem <robert.van.v@gmail.com>
2018-02-14 16:45:46 +0000

    Added initial MA parameters Fixed readability of the code

--- Testcase/DMITestCases/17 Train Speed/17.5/17.5.4 Circular_Speed_Gauge_removal_when_received_an_invalid_value_of_EVC_1_and_EVC_7.cs
+++ Testcase/DMITestCases/17 Train Speed/17.5/17.5.4 Circular_Speed_Gauge_removal_when_received_an_invalid_value_of_EVC_1_and_EVC_7.cs
@@ -30,51 +30,60 @@ namespace Testcase.DMITestCases
             // Testcase entrypoint
 
             StartUp();
-            DmiActions.Complete_SoM_L1_SR(this);
+            DmiActions.Complete_SoM_L1_FS(this);
 
             MakeTestStepHeader(1, UniqueIdentifier++,
-                "Drive the train forward pass BG1 with speed = 30km/h.Then, stop the train",
+                "Drive the train forward pass BG1 with speed = 30 km/h.Then, stop the train",
                 "DMI displays in FS mode, Level 1");
             /*
             Test Step 1
-            Action: Drive the train forward pass BG1 with speed = 30km/h.Then, stop the train
+            Action: Drive the train forward pass BG1 with speed = 30 km/h. Then, stop the train
             Expected Result: DMI displays in FS mode, Level 1
             */
-            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
-            EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 30;
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. DMI displays in FS mode, Level 1.");
 
+            // Set up initial MA parameters taken from .tdg file
             EVC1_MMIDynamic.MMI_V_TRAIN = 0;
+            EVC1_MMIDynamic.MMI_V_PERMITTED_KMH = 160;
+            EVC1_MMIDynamic.MMI_V_RELEASE_KMH = -1;
+            EVC1_MMIDynamic.MMI_V_TARGET = -1;
 
             MakeTestStepHeader(2, UniqueIdentifier++,
                 "Use the test script file 12_5_4_a.xml to send EVC-1 with MMI_M_WARNING = 7",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard");
             /*
             Test Step 2
             Action: Use the test script file 12_5_4_a.xml to send EVC-1 with MMI_M_WARNING = 7
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed gauge is re-appear when DMI received packet EVC-1 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: MMI_M_WARNING);
             */
             XML_12_5_4(msgType.typea);
-
-            Wait_Realtime(500); // this was not working each time
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1.The Circular Speed Gauge is removed from sub-area B2.");
 
             // correct the invalid item:
-            EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Overspeed_Status_Ceiling_Speed_Monitoring;
+            EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_Ceiling_Speed_Monitoring;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1.The Circular Speed Gauge re-appears in sub-area B2.");
+
             MakeTestStepHeader(3, UniqueIdentifier++,
                 "Use the test script file 12_5_4_b.xml to send EVC-1 with,MMI_V_TARGET = 11112",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard.");
             /*
             Test Step 3
             Action: Use the test script file 12_5_4_b.xml to send EVC-1 with,MMI_V_TARGET = 11112
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: MMI_V_TARGET);
             */
             XML_12_5_4(msgType.typeb);
@@ -84,16 +93,21 @@ namespace Testcase.DMITestCases
 
             // correct the invalid item:
             EVC1_MMIDynamic.MMI_V_TARGET = 1111;
+
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1.The Circular Speed Gauge re-appears in sub-area B2.");
 
             MakeTestStepHeader(4, UniqueIdentifier++,
                 "Use the test script file 12_5_4_c.xml to send EVC-1 with,MMI_V_PERMITTED = 11112",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard");
             /*
             Test Step 4
             Action: Use the test script file 12_5_4_c.xml to send EVC-1 with,MMI_V_PERMITTED = 11112
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: MMI_V_PERMITTED);
             */
             XML_12_5_4(msgType.typec);
@@ -109,11 +123,15 @@ namespace Testcase.DMITestCases
 
             MakeTestStepHeader(5, UniqueIdentifier++,
                 "Use the test script file 12_5_4_d.xml to send EVC-1 with,MMI_V_INTERVENTION = 11112",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard");
             /*
             Test Step 5
             Action: Use the test script file 12_5_4_d.xml to send EVC-1 with,MMI_V_INTERVENTION = 11112
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: MMI_V_INTERVENTION);
             */
             XML_12_5_4(msgType.typed);
@@ -129,11 +147,15 @@ namespace Testcase.DMITestCases
 
             MakeTestStepHeader(6, UniqueIdentifier++,
                 "Use the test script file 12_5_4_e.xml to send EVC-1 with,MMI_V_RELEASE = 11112",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard");
             /*
             Test Step 6
             Action: Use the test script file 12_5_4_e.xml to send EVC-1 with,MMI_V_RELEASE = 11112
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed guage is re-appear when DMI received packet EVC-1 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: MMI_V_RELEASE);
             */
             XML_12_5_4(msgType.typee);
@@ -150,11 +172,15 @@ namespace Testcase.DMITestCases
 
             MakeTestStepHeader(7, UniqueIdentifier++,
                 "Use the test script file 12_5_4_f.xml to send EVC-7 with,OBU_TR_M_MODE = 17",
-                "Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-7 from ETCS onboard");
+                "Verify the following information," +
+                "(1) The Circular Speed Gauge is removed from sub-area B2." +
+                "Note: The circular speed gauge re-appears when DMI received packet EVC-1 from ETCS onboard");
             /*
             Test Step 7
             Action: Use the test script file 12_5_4_f.xml to send EVC-7 with,OBU_TR_M_MODE = 17
-            Expected Result: Verify the following information,(1)   The Circular Speed Gauge is removed from sub-area B2.Note: The ciruclar speed guage is re-appear when DMI received packet EVC-7 from ETCS onboard
+            Expected Result: Verify the following information,
+            (1) The Circular Speed Gauge is removed from sub-area B2.
+            Note: The circular speed guage is re-appear when DMI received packet EVC-7 from ETCS onboard
             Test Step Comment: (1) MMI_gen 977 (partly: OBU_TR_M_MODE);
             */
             XML_12_5_4(msgType.typef);
@@ -162,6 +188,7 @@ namespace Testcase.DMITestCases
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1.The Circular Speed Gauge is removed from sub-area B2.");
 
+            // correct the invalid item:
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
@@ -200,14 +227,12 @@ namespace Testcase.DMITestCases
                 EVC1_MMIDynamic.MMI_V_TARGET = 0;
                 EVC1_MMIDynamic.MMI_V_PERMITTED = 4444;
                 EVC1_MMIDynamic.MMI_V_RELEASE = 0;
-                EVC1_MMIDynamic.MMI_O_BRAKETARGET = 0x3ba1a259;
-                EVC1_MMIDynamic.MMI_O_IML = 0x3b9b4523;
+                EVC1_MMIDynamic.MMI_O_BRAKETARGET = 0x3ba1a259;     // 1000448601
+                EVC1_MMIDynamic.MMI_O_IML = 0x3b9b4523;             // 1000031523
                 EVC1_MMIDynamic.MMI_V_INTERVENTION = 4658;
 
-                SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0x13;
-                SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0xff;
-                //SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity1.Value = 4415; // All validity bits set
-                //SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity2.Value = 63;   // All validity bits set          
+                SITR.ETCS1.Dynamic.EVC01Validity1.Value = 0xc800;
+                SITR.ETCS1.Dynamic.EVC01Validity2.Value = 0xff00;        
             }
 
             switch (type)
@@ -215,32 +240,32 @@ namespace Testcase.DMITestCases
                 case msgType.typea:
                     EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Spare; // 7
                     EVC1_MMIDynamic.MMI_V_TRAIN = 0;
-
                     break;
+
                 case msgType.typeb:
-                    EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Warning_Status_Ceiling_Speed_Monitoring; // 4
+                    EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_Ceiling_Speed_Monitoring; // 0
                     EVC1_MMIDynamic.MMI_V_TARGET = 11112; // invalid
                     EVC1_MMIDynamic.MMI_V_TRAIN = 0;
-
                     break;
+
                 case msgType.typec:
                     EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_Ceiling_Speed_Monitoring; // 0
                     EVC1_MMIDynamic.MMI_V_PERMITTED = 11112; //invalid
                     EVC1_MMIDynamic.MMI_V_TRAIN = 0;
-
                     break;
+
                 case msgType.typed:
                     EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_Ceiling_Speed_Monitoring; // 0
                     EVC1_MMIDynamic.MMI_V_INTERVENTION = 11112; // invalid
                     EVC1_MMIDynamic.MMI_V_TRAIN = 0;
-
                     break;
+
                 case msgType.typee:
                     EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_Ceiling_Speed_Monitoring; // 0
                     EVC1_MMIDynamic.MMI_V_RELEASE = 11112; // invalid
                     EVC1_MMIDynamic.MMI_V_TRAIN = 0;
-
                     break;
+
                 case msgType.typef:
                     EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_EBTestInProgress = 0;
                     EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_EB_Status = 0;
@@ -257,8 +282,8 @@ namespace Testcase.DMITestCases
                     EVC7_MMIEtcsMiscOutSignals.BRAKE_TEST_TIMEOUT = 46;
                     EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_O_TRAIN = 1000010000;
 
-                    SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity1.Value = 0x113f; // All validity bits set
-                    SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity2.Value = 0x3f; // All validity bits set
+                    SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity1.Value = 0xfc88; // Bit-reverse of 4415 as set by xml file
+                    SITR.ETCS1.EtcsMiscOutSignals.EVC7Validity2.Value = 0xfc00; // Bit-reverse of 63 as set by xml file
 
                     break;
             }
