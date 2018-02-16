6a32ecf97b6a4b23162eda2a9657467604ce3d89
Robert van Vaardegem <robert.van.v@gmail.com>
2018-02-16 14:41:58 +0000

    Test case has been commented out since it is a repeat of TC_12_12

--- Testcase/DMITestCases/17 Train Speed/17.15/17.15 Slip_and_Slide_are_configure_to_0_at_the_same_time.cs
+++ Testcase/DMITestCases/17 Train Speed/17.15/17.15 Slip_and_Slide_are_configure_to_0_at_the_same_time.cs
@@ -27,9 +27,9 @@ namespace Testcase.DMITestCases
             // This identifier shall match the identity of the first testcasestep of the testcase in Doors
             UniqueIdentifier = 21850;
 
-            StartUp();
-
-            DmiActions.Complete_SoM_L1_SR(this);
+            // StartUp();
+            // 
+            // DmiActions.Complete_SoM_L1_SR(this);
 
             // Testcase entrypoint
             TraceInfo("This test case requires an ATP configuration change - " +
@@ -42,23 +42,27 @@ namespace Testcase.DMITestCases
             Action: Driver the train forward
             Expected Result: DMI changes from SR mode to FS mode
             */
-            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
-            EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 5;
+            TraceInfo("This test is a repeat of a previous test case. Step skipped.");
+            // EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
+            // EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 5;
 
             // Call generic Check Results Method
-            DmiExpectedResults.FS_mode_displayed(this);
+            // DmiExpectedResults.FS_mode_displayed(this);
+            // 
+            // MakeTestStepHeader(2, UniqueIdentifier++, "Drive the train forward with speed = 140 km/h",
+            //     "The speed pointer is displayed with speed =140");
 
-            MakeTestStepHeader(2, UniqueIdentifier++, "Drive the train forward with speed = 140 km/h",
-                "The speed pointer is displayed with speed =140");
             /*
             Test Step 2
             Action: Drive the train forward with speed = 140 km/h
             Expected Result: The speed pointer is displayed with speed =140
             */
-            EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 140;
+            TraceInfo("This test is a repeat of a previous test case. Step skipped.");
 
-            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
-                                "1. The speed pointer is displayed with speed = 140 km/h.");
+            // EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 140;
+            // 
+            // WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+            //                     "1. The speed pointer is displayed with speed = 140 km/h.");
 
             MakeTestStepHeader(3, UniqueIdentifier++,
                 "Use the test script file 12_15_a.xml to send EVC-1 with,MMI_M_SLIP = 1MMI_M_SLIDE =0",
@@ -69,10 +73,12 @@ namespace Testcase.DMITestCases
             Expected Result: Verify that Slip and Slide indicator are not display on DMI
             Test Step Comment: (1) MMI_gen 1692 (partly: ETC speed, disabled);   
             */
-            XML_12_15(msgType.typea);
+            TraceInfo("This test is a repeat of a previous test case. Step skipped.");
 
-            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
-                                "1. Slip and Slide indicator are not displayed on DMI.");
+            // XML_12_15(msgType.typea);
+            // 
+            // WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+            //                     "1. Slip and Slide indicator are not displayed on DMI.");
 
             MakeTestStepHeader(4, UniqueIdentifier++,
                 "Use the test script file 12_15_b.xml to send EVC-1 with,MMI_M_SLIP = 0MMI_M_SLIDE = 1",
@@ -83,10 +89,12 @@ namespace Testcase.DMITestCases
             Expected Result: Verify that Slip and Slide indicator are not display on DMI
             Test Step Comment: (1) MMI_gen 1692 (partly: ETC speed, disabled);   
             */
-            XML_12_15(msgType.typeb);
+            TraceInfo("This test is a repeat of a previous test case. Step skipped.");
 
-            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
-                                "1. Slip and Slide indicator are not displayed on DMI.");
+            // XML_12_15(msgType.typeb);
+            // 
+            // WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+            //                     "1. Slip and Slide indicator are not displayed on DMI.");
 
             MakeTestStepHeader(5, UniqueIdentifier++,
                 "Use the test script file 12_15_c.xml to send EVC-1 with,MMI_M_SLIP = 1MMI_M_SLIDE = 1",
@@ -97,10 +105,12 @@ namespace Testcase.DMITestCases
             Expected Result: Verify that Slip and Slide indicator are not display on DMI
             Test Step Comment: (1) MMI_gen 1692 (partly: ETC speed, disabled);   
             */
-            XML_12_15(msgType.typec);
+            TraceInfo("This test is a repeat of a previous test case. Step skipped.");
 
-            WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
-                                "1. Slip and Slide indicator are not displayed on DMI.");
+            // XML_12_15(msgType.typec);
+            // 
+            // WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
+            //                     "1. Slip and Slide indicator are not displayed on DMI.");
 
             TraceHeader("End of test");
 
