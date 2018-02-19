fb837914b5d8201c91eadc684bb7cc80a5d3990a
Robert van Vaardegem <robert.van.v@gmail.com>
2018-02-19 15:38:09 +0000

    Aligned to TC_15_2_11

--- Testcase/DMITestCases/20 Status Information to The Driver/20.2/20.2.13 ETCS_Level_ETCS_Level_Transitions_by_receiving_data_packet_from_ETCS_Onboard_LNTC_L3.cs
+++ Testcase/DMITestCases/20 Status Information to The Driver/20.2/20.2.13 ETCS_Level_ETCS_Level_Transitions_by_receiving_data_packet_from_ETCS_Onboard_LNTC_L3.cs
@@ -44,12 +44,8 @@ namespace Testcase.DMITestCases
             Expected Result: DMI displays in ATB STM mode, Level NTC
             */
             // Call generic Action Method
-
-            DmiActions.ShowInstruction(this, "Power on the system");
-
             StartUp();
             DmiActions.Set_Driver_ID(this, "1234");
-            // Skip brake test...
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.NationalSystem;
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Level = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_LEVEL.LNTC;
             DmiActions.Finished_SoM_Default_Window(this);
@@ -68,7 +64,7 @@ namespace Testcase.DMITestCases
             */
             EVC8_MMIDriverMessage.MMI_Q_TEXT_CLASS = MMI_Q_TEXT_CLASS.ImportantInformation;
             EVC8_MMIDriverMessage.MMI_I_TEXT = 1;
-            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 3;
+            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 1;
             EVC8_MMIDriverMessage.PlainTextMessage = "3";
             EVC8_MMIDriverMessage.MMI_Q_TEXT = 276;
             EVC8_MMIDriverMessage.Send();
@@ -84,8 +80,7 @@ namespace Testcase.DMITestCases
             Expected Result: DMI displays LE15 symbol in sub-area C1
             Test Step Comment: MMI_gen 9431 (partly: LE15); 
             */
-            // Spec says LE13 (displayed)
-            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 1;
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 2;
             EVC8_MMIDriverMessage.MMI_Q_TEXT = 257;
             EVC8_MMIDriverMessage.Send();
 
@@ -93,18 +88,22 @@ namespace Testcase.DMITestCases
                                 "1. DMI displays the Level transition symbol, LE15, in sub-area C1.");
 
             MakeTestStepHeader(4, UniqueIdentifier++, "Press acknowledgement LE15 symbol in sub-area C1",
-                "Verify the following information,(1)    DMI replaces LE15 symbol with LE14 in sub-area C1.(2)    Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 9 (Ack level 3)");
+                "Verify the following information," +
+                "(1) DMI replaces LE15 symbol with LE14 in sub-area C1." +
+                "(2) Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 9 (Ack level 3)");
             /*
             Test Step 4
             Action: Press acknowledgement LE15 symbol in sub-area C1
-            Expected Result: Verify the following information,(1)    DMI replaces LE15 symbol with LE14 in sub-area C1.(2)    Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 9 (Ack level 3)
+            Expected Result: Verify the following information,
+            (1) DMI replaces LE15 symbol with LE14 in sub-area C1.
+            (2) Use the log file to confirm that DMI sends out packet [MMI_DRIVER_ACTION (EVC-152)] with the value of variable MMI_M_DRIVER_ACTION refer to sequence below,a)   MMI_M_DRIVER_ACTION = 9 (Ack level 3)
             Test Step Comment: (1) MMI_gen 9431 (partly: LE14);(2) MMI_gen 11470 (partly: Bit #9);
             */
             DmiActions.ShowInstruction(this, "Press in sub-area C1 to acknowledge the Level transition symbol, LE15");
 
             EVC152_MMIDriverAction.Check_MMI_M_DRIVER_ACTION = EVC152_MMIDriverAction.MMI_M_DRIVER_ACTION.Level3Ack;
 
-            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 3;
+            EVC8_MMIDriverMessage.MMI_I_TEXT = 3;
             EVC8_MMIDriverMessage.MMI_Q_TEXT = 276;
             EVC8_MMIDriverMessage.Send();
 
@@ -120,7 +119,6 @@ namespace Testcase.DMITestCases
             */
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Level = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_LEVEL.L3;
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
-            EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 4;
             EVC8_MMIDriverMessage.Send();
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
