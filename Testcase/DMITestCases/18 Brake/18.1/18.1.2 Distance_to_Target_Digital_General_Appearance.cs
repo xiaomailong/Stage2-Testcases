3999d584d08db58e2a8c7f9d2bd1b4fb8f35d6e3
Robert van Vaardegem <robert.van.v@gmail.com>
2018-02-28 18:27:14 +0000

    Added "ID" in class name Added button press verification at Trip Ack press.

--- Testcase/DMITestCases/18 Brake/18.1/18.1.2 Distance_to_Target_Digital_General_Appearance.cs
+++ Testcase/DMITestCases/18 Brake/18.1/18.1.2 Distance_to_Target_Digital_General_Appearance.cs
@@ -1,4 +1,5 @@
 using System;
+using Testcase.Telegrams.DMItoEVC;
 using Testcase.Telegrams.EVCtoDMI;
 
 
@@ -19,7 +20,7 @@ namespace Testcase.DMITestCases
     /// Used files:
     /// 13_1_2.tdg
     /// </summary>
-    public class TC_13_1_2_Brake : TestcaseBase
+    public class TC_ID_13_1_2_Brake : TestcaseBase
     {
         public override bool TestcaseEntryPoint()
         {
@@ -42,13 +43,14 @@ namespace Testcase.DMITestCases
             EVC1_MMIDynamic.MMI_V_PERMITTED_KMH = 10;
 
             // ?? Set an EOA so the DMI can display a target
-            EVC1_MMIDynamic.MMI_O_BRAKETARGET = 60000; // 600m
-            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_O_TRAIN = 1000; // 100m
+            EVC1_MMIDynamic.MMI_O_BRAKETARGET = 60000; // 600 m
+            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_O_TRAIN = 1000; // 100 m
 
             // Does the DMI automatically switch to FS mode??
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.FullSupervision;
 
             EVC1_MMIDynamic.MMI_M_WARNING = MMI_M_WARNING.Normal_Status_PreIndication_Monitoring;
+
             MakeTestStepHeader(2, UniqueIdentifier++,
                 "Continue to drive the train forward pass EOA until entering TR mode",
                 "Verify the following information,The distance to target digital is not shown in sub-area A2.Use the log file to confirm that DMI received packet information EVC-1 with vairable MMI_O_BRAKETARGET < 0.Use the log file to confirm that DMI received packet information EVC-7 with following variables,OBU_TR_M_MODE (EVC-7) = 7 (Trip)");
@@ -58,13 +60,13 @@ namespace Testcase.DMITestCases
             Expected Result: Verify the following information,The distance to target digital is not shown in sub-area A2.Use the log file to confirm that DMI received packet information EVC-1 with vairable MMI_O_BRAKETARGET < 0.Use the log file to confirm that DMI received packet information EVC-7 with following variables,OBU_TR_M_MODE (EVC-7) = 7 (Trip)
             Test Step Comment: (1) MMI_gen 6774 (partly: when MMI_O_BRAKETARGET less than zero);(2) MMI_gen 6774 (partly: received MMI_O_BRAKETARGET less than zero);(3) MMI_gen 2567 (partly: Table 38 ‘TR mode’);
             */
-            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_O_TRAIN = 70000; // 700m
+            EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_O_TRAIN = 70000; // 700 m
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.Trip;
 
             WaitForVerification("Check the following:" + Environment.NewLine + Environment.NewLine +
                                 "1. The digital distance to target is not displayed in sub-area A2.");
 
-            MakeTestStepHeader(3, UniqueIdentifier++, "Stop the train.Then, acknowledge TR mode by press a sub-area C1",
+            MakeTestStepHeader(3, UniqueIdentifier++, "Stop the train. Then, acknowledge TR mode by press a sub-area C1",
                 "DMI displays in PT mode, Level 1.Verify the following information,The distance to target digital is not shown in sub-area A2.Use the log file to confirm that DMI received packet information EVC-1 with vairable MMI_O_BRAKETARGET < 0.Use the log file to confirm that DMI received packet information EVC-7 with following variables,OBU_TR_M_MODE (EVC-7) = 8 (Post Trip)");
             /*
             Test Step 3
@@ -73,6 +75,7 @@ namespace Testcase.DMITestCases
             Test Step Comment: (1) MMI_gen 6774 (partly: when MMI_O_BRAKETARGET less than zero);(2) MMI_gen 6774 (partly: received MMI_O_BRAKETARGET is less than zero);(3) MMI_gen 2567 (partly: Table 38 'PT mode');
             */
             EVC1_MMIDynamic.MMI_V_TRAIN_KMH = 0;
+
             EVC8_MMIDriverMessage.MMI_I_TEXT = 1;
             EVC8_MMIDriverMessage.MMI_Q_TEXT_CLASS = MMI_Q_TEXT_CLASS.ImportantInformation;
             EVC8_MMIDriverMessage.MMI_Q_TEXT_CRITERIA = 1;
@@ -80,6 +83,8 @@ namespace Testcase.DMITestCases
             EVC8_MMIDriverMessage.Send();
 
             DmiActions.ShowInstruction(this, "Acknowledge Trip mode by pressing sub-area C1");
+
+            EVC152_MMIDriverAction.Check_MMI_M_DRIVER_ACTION = EVC152_MMIDriverAction.MMI_M_DRIVER_ACTION.TrainTripAck;
 
             EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_Mode = EVC7_MMIEtcsMiscOutSignals.MMI_OBU_TR_M_MODE.PostTrip;
             WaitForVerification(
